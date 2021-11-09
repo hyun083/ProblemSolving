835bff30cd1a1a1be4eec58a33a29eee32f193f7
이정현 <jh9501@gmail.com>
2021-11-09 19:49:16 +0900

    2021-11-09

---
+++ 11650/11650_cpp/cmake-build-debug/CMakeFiles/11650_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "11650_cpp"
+  "11650_cpp.pdb"
+  "CMakeFiles/11650_cpp.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11650_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
