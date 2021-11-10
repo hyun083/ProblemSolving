7be17af2acf85404a7c49f132180c65b7b4ba89b
이정현 <jh9501@gmail.com>
2021-11-11 00:33:43 +0900

    2021-11-11

---
+++ 1181/1181_cpp/cmake-build-debug/CMakeFiles/1181_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1181_cpp"
+  "1181_cpp.pdb"
+  "CMakeFiles/1181_cpp.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1181_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1181/1181_cpp/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+bool compare(string a, string b){
+    if(a.size() == b.size()){
+        for(int i=0; i<a.size(); i++){
+            if(a[i] != b[i]) return a[i] < b[i];
+        }
+        return a < b;
+    }else return a.size() < b.size();
+}
+int main() {
+    cin >> N;
+    vector<string> arr;
+    for(int i=0; i<N; i++){
+        string str;
+        cin >> str;
+        arr.push_back(str);
+    }
+    sort(arr.begin(),arr.end(),compare);
+    arr.erase(unique(arr.begin(),arr.end()),arr.end());
+    for(vector<string>::iterator iter = arr.begin(); iter!=arr.end(); ++iter){
+        cout << *iter << '\n';
+    }
+    return 0;
+}
