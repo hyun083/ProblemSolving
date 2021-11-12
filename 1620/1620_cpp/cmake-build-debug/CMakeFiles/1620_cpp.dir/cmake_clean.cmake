4022a315d41ee693a2beb6982454caa319d95eb1
이정현 <jh9501@gmail.com>
2021-11-12 23:05:48 +0900

    2021-11-12

---
+++ 1620/1620_cpp/cmake-build-debug/CMakeFiles/1620_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1620_cpp"
+  "1620_cpp.pdb"
+  "CMakeFiles/1620_cpp.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1620_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4153/4153_swift/4153_swift/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  4153_swift
+//
+//  Created by Hyun on 2021/11/12.
+//
+
+import Foundation
+func main() -> Int{
+    while(true){
+        let inputs = readLine()!.split(separator: " ").map{Int(String($0))! * Int(String($0))!}
+        let max = inputs.max()!
+        let min = inputs.min()!
+        let mid = inputs.filter({$0 != max && $0 != min})
+        if (inputs[0] == 0){
+            return 0
+        }else if(mid.first == max - min){
+            print("right")
+        }else{
+            print("wrong")
+        }
+    }
+}
+
+main()
