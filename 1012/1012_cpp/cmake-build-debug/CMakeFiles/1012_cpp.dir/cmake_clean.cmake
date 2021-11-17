93a764e72e10cf99c1512dbe9f95589b178de036
이정현 <jh9501@gmail.com>
2021-11-18 01:16:53 +0900

    2021-11-18

---
+++ 1012/1012_cpp/cmake-build-debug/CMakeFiles/1012_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1012_cpp"
+  "1012_cpp.pdb"
+  "CMakeFiles/1012_cpp.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1012_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
