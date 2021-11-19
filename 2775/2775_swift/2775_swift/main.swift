828ebcdfe7199b19083b98d2dd432dbb9612bb91
이정현 <jh9501@gmail.com>
2021-11-19 21:55:35 +0900

    2021-11-19

---
+++ 2775/2775_swift/2775_swift/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  2775_swift
+//
+//  Created by Hyun on 2021/11/19.
+//
+
+import Foundation
+
+func getCount(_ k:Int, _ n:Int) -> Int{
+    if k == 0 {return n}
+    if n == 1 {return 1}
+    return getCount(k-1, n) + getCount(k, n-1)
+}
+
+let T = Int(readLine()!)!
+
+for _ in 1...T{
+    let k = Int(readLine()!)!
+    let n = Int(readLine()!)!
+    print(getCount(k, n))
+}
