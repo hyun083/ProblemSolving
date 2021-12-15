573df3cc7fa518517594932ad7869a217a3e5a6b
hyun083 <jh9501@gmail.com>
2021-12-16 02:55:43 +0900

    2021-12-16

--- 1676/1676_swift/1676_swift/main.swift
+++ 1676/1676_swift/1676_swift/main.swift
@@ -8,4 +8,18 @@
 import Foundation
 
 let N = Int(readLine()!)!
-print(N/5)
+var cnt = 0
+if N != 0{
+    for i in 1...N{
+        if i%5 == 0{
+            cnt += 1
+        }
+        if i%25 == 0{
+            cnt += 1
+        }
+        if i%125 == 0{
+            cnt += 1
+        }
+    }
+}
+print(cnt)
