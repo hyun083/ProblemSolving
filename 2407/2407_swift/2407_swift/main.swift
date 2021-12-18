9e4ab2cdf0827bf57cf2288bf5545d83d86ec5d4
hyun083 <jh9501@gmail.com>
2021-12-18 23:39:11 +0900

    2021-12-18

---
+++ 2407/2407_swift/2407_swift/main.swift
@@ -0,0 +1,55 @@
+//
+//  main.swift
+//  2407_swift
+//
+//  Created by Hyun on 2021/12/18.
+//
+
+import Foundation
+
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+var N = NM[0]
+var R = NM[1]
+var dp = Array(repeating: Array(repeating: "", count: 101), count: 101)
+
+func stringAdd(numb1:String, numb2:String)->String{
+    var total = [String]()
+    var carry = "0"
+    
+    var num1 = numb1
+    var num2 = numb2
+    
+    while(!num1.isEmpty && !num2.isEmpty && carry == "0"){
+        if num1.isEmpty{
+            let num = Int(String(num2.removeLast()))! + Int(carry)!
+            total.append(String(num%10))
+            carry = String(num/10)
+            continue
+        }
+        if num2.isEmpty{
+            let num = Int(String(num1.removeLast()))! + Int(carry)!
+            total.append(String(num%10))
+            carry = String(num/10)
+            continue
+        }
+        let num = Int(String(num1.removeLast()))! + Int(String(num2.removeLast()))! + Int(carry)!
+        total.append(String(num%10))
+        carry = String(num/10)
+    }
+    
+    return total.reversed().joined()
+}
+
+func combination(n:Int, m:Int) -> String{
+    if n==m || m==0{
+        dp[n][m] = "1"
+        return dp[n][m]
+    }else if dp[n][m] != ""{
+        return dp[n][m]
+    }else{
+        dp[n][m] = stringAdd(numb1: combination(n: n-1, m: m-1), numb2: combination(n: n-1, m: m))
+        return dp[n][m]
+    }
+}
+print(combination(n: N, m: R))
+print(dp)
---
+++ 7662/7662_swift/7662_swift/main.swift
@@ -0,0 +1,103 @@
+//
+//  main.swift
+//  7662_swift
+//
+//  Created by Hyun on 2021/12/18.
+//
+
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let f = FileIO()
+let T = f.readInt()
+var ans = Array(repeating: [Int](), count: T)
+
+for i in 0..<T{
+    let N = f.readInt()
+
+    for _ in 1...N{
+        let command = f.readString()
+        let number = f.readInt()
+        if command == "I"{
+            ans[i].append(number)
+        }else{
+            if ans[i].count >= 1{
+                if number == 1{
+                    ans[i].sort(by: <)
+                }else{
+                    ans[i].sort(by: >)
+                }
+                ans[i].removeLast()
+            }else{
+                continue
+            }
+        }
+    }
+}
+
+for i in 0..<T{
+    if ans[i].count < 1{
+        print("EMPTY")
+    }else{
+        ans[i].sort()
+        print("\(ans[i].last!) \(ans[i].first!)")
+    }
+}
+
