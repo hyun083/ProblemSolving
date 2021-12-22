588501898d272f0c02899286b2017360d9beedaa
hyun083 <jh9501@gmail.com>
2021-12-22 21:45:17 +0900

    2021-12-22

--- 1629/1629_swift/1629_swift/main.swift
+++ 1629/1629_swift/1629_swift/main.swift
@@ -14,16 +14,22 @@ var B = numbers[1]
 let C = numbers[2]
 
 func p(num1:UInt64, num2:UInt64) -> UInt64{
-    if B == 0{
+    if num2 == 0{
         return UInt64(1)
-    }else if B == 1{
+    }else if num2 == 1{
         return num1
-    }else{
-        if B % 2 == 0{
-            return 
-        }
     }
     
+    //홀수일경우
+    if num2 % 2 > 0{
+        return p(num1: num1, num2: num2-1)*num1
+    }
+    
+    //짝수일경우 모듈러공식 (a * b) mod n = ((a mod n) * (b mod n)) mod n
+    var half = p(num1: num1, num2: num2/2)
+    half %= C
+    return (half*half)%C
 }
 
+print(p(num1: A, num2: B)%C)
 
