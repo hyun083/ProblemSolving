4e8bbb18f1ed6c68b2612b57823d844a3f4cfb73
hyun083 <jh9501@gmail.com>
2021-12-27 22:51:36 +0900

    2021-12-27

---
+++ 11053/11053_swift/11053_swift/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  11053_swift
+//
+//  Created by Hyun on 2021/12/27.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+var dp = Array(repeating: 1, count: N)
+
+for i in 0..<N{
+    dp[i] = 1
+    for k in 0..<i{
+        if arr[k] < arr[i]{
+            dp[i] = max(dp[i], dp[k]+1)
+        }
+    }
+}
+print(dp.max()!)
