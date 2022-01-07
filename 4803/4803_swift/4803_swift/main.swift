008ff6e4d5e74a2d4681afcefa169a1dac05117a
hyun083 <jh9501@gmail.com>
2022-01-07 15:58:06 +0900

    2022-01-07

---
+++ 4803/4803_swift/4803_swift/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  4803_swift
+//
+//  Created by Hyun on 2022/01/07.
+//
+
+import Foundation
+
+while(true){
+    let T = readLine()!.split(separator: " ").map{Int(String($0))!}
+    if T[0]==0 && T[1]==0{
+        break
+    }
+    
+}
+
