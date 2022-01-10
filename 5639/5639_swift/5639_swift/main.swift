476fd278c8ff8751de9b44d62c1b0defa09cad16
hyun083 <jh9501@gmail.com>
2022-01-10 20:48:37 +0900

    2022-01-10

---
+++ 5639/5639_swift/5639_swift/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  5639_swift
+//
+//  Created by Hyun on 2022/01/10.
+//
+
+import Foundation
+
+var arr = [Int]()
+
+while let input = readLine(){
+    arr.append(Int(input)!)
+}
+var tree = Array(repeating: <#T##_#>, count: <#T##Int#>)
+for i in 0..<arr.count{
+    
+}
+
+func tree(p:Int, c:Int){
+    if p < c && {
+        
+    }
+}
+
+print(arr)
+
