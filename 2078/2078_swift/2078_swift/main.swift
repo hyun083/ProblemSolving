305954e7e6db898809f70651ca79906b7a465586
hyun083 <jh9501@gmail.com>
2022-01-19 16:55:48 +0900

    2022-01-19

--- 2078/2078_swift/2078_swift/main.swift
+++ 2078/2078_swift/2078_swift/main.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
-
-
+var A = input[0]
+var B = input[1]
+var left = 0
+var right = 0
+while true{
+    if A == 1{
+        right += B-1
+        break
+    }else if B == 1{
+        left += A-1
+        break
+    }
+    if A>B{
+        left += A/B
+        A %= B
+    }else{
+        right += B/A
+        B %= A
+    }
+}
+print(left, right)
