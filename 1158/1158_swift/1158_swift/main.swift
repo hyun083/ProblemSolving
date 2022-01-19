d50fa9fc110d5c58af44342168081453cc469489
hyun083 <jh9501@gmail.com>
2022-01-20 01:25:44 +0900

    2022-01-20

---
+++ 1158/1158_swift/1158_swift/main.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  1158_swift
+//
+//  Created by Hyun on 2022/01/20.
+//
+
+import Foundation
+
+let NK = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NK[0]
+let K = NK[1]
+
+var arr = Array(1...N)
+var josephus = [Int]()
+
+var cnt = 0
+var idx = -1
+
+while josephus.count < arr.count{
+    cnt += 1
+    idx += 1
+    if idx >= arr.count{
+        idx = 0
+    }
+    if arr[idx] == 0{
+        while arr[idx] == 0{
+            idx += 1
+            if idx >= arr.count{
+                idx = 0
+            }
+        }
+    }
+    if cnt == K{
+        josephus.append(arr[idx])
+        arr[idx]=0
+        cnt = 0
+        continue
+    }
+}
+
+var text = "<"
+for j in josephus{
+    if j == josephus.last{
+        text+=String(j)
+    }else{
+        text+=String(j)+", "
+    }
+}
+text += ">"
+print(text)
