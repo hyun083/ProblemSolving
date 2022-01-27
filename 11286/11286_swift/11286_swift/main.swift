b0fc1f990514de74a1c981fc10c806f6998082e0
hyun083 <jh9501@gmail.com>
2022-01-27 17:30:31 +0900

    2022-01-27

---
+++ 11286/11286_swift/11286_swift/main.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  11286_swift
+//
+//  Created by Hyun on 2022/01/27.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+var arr = [Int:[Int]]()
+var result = ""
+
+for _ in 1...N{
+    let number = Int(readLine()!)!
+    if number == 0{
+        if arr.isEmpty{
+            result += "0\n"
+        }else{
+            let keyIdx = arr.keys.min()!
+            let minimumElement = arr[keyIdx]!.min()!
+            let minimumElementIdx = arr[keyIdx]!.firstIndex(of: minimumElement)!
+            
+            result += String(arr[keyIdx]!.remove(at: minimumElementIdx)) + "\n"
+            if arr[keyIdx] == []{
+                arr.removeValue(forKey: keyIdx)
+            }
+        }
+    }else{
+        let key = abs(number)
+        if arr[key] != nil {
+            arr[key]!.append(number)
+        }else{
+            arr[key] = [number]
+        }
+    }
+}
+print(result)
