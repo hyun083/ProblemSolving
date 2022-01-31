2d711c7eb526fac43a8d759ca6a08edf00a9a0e0
hyun083 <jh9501@gmail.com>
2022-01-31 22:00:08 +0900

    2022-01-31

--- 1300/1300_cpp/main.cpp
+++ 1300/1300_cpp/main.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    vector<int> arr;
-    int N,M; bool flag = false;
+    priority_queue<int,vector<int>,less<int>> pq;
+    int N,M;
     cin >> N;
     cin >> M;
 
     for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            if(arr.size() < M){
-                arr.push_back(i*j);
-            }else{
-                flag = true;
-                break;
+        for(int j=1; j<=N; j++) {
+            pq.push(i*j);
+            if(pq.size() > M){
+                pq.pop();
             }
         }
-        if(flag){break;}
     }
-    sort(arr.begin(),arr.end());
-
-    cout << arr.back();
+    cout << pq.top();
     return 0;
 }
