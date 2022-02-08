10de43dd95053fe6e557d22d63f881fb3c0e3517
hyun083 <jh9501@gmail.com>
2022-02-09 01:58:25 +0900

    2022-02-09

---
+++ 1271/1271_swift/1271_swift/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  1271_swift
+//
+//  Created by Hyun on 2022/02/09.
+//
+
+import Foundation
+
+let NM = readLine()!.split(separator: " ").map{String($0)}
---
+++ 1550/1550_swift/1550_swift/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  1550_swift
+//
+//  Created by Hyun on 2022/02/09.
+//
+
+//import Foundation
+
+print(Int(readLine()!,radix: 16)!)
