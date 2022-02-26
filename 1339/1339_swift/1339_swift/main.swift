761f1f217cfffed926ebbe33c203176fda180d3c
hyun083 <jh9501@gmail.com>
2022-02-26 20:12:31 +0900

    2022-02-26

---
+++ 1339/1339_swift/1339_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1339_swift
+//
+//  Created by Hyun on 2022/02/26.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+
---
+++ 2864/2864_swift/2864_swift/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  2864_swift
+//
+//  Created by Hyun on 2022/02/26.
+//
+
+import Foundation
+
+let numbers = readLine()!.split(separator: " ").map{String($0)}
+var num1 = ""
+var num2 = ""
+
+for number in numbers {
+    for n in number{
+        if n == "5" || n == "6"{
+            num1 += "5"
+            num2 += "6"
+            continue
+        }
+        num1 += String(n)
+        num2 += String(n)
+    }
+    num1 += " "
+    num2 += " "
+}
+print(num1.split(separator: " ").map{Int(String($0))!}.reduce(0, +),terminator: " ")
+print(num2.split(separator: " ").map{Int(String($0))!}.reduce(0, +))
