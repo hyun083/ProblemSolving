12a0ae833a079a4d3e08ed3036d4f8a0366529c6
hyun083 <jh9501@gmail.com>
2022-02-28 23:24:58 +0900

    2022-02-28

---
+++ 1449/1449_swift/1449_swift/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  1449_swift
+//
+//  Created by Hyun on 2022/02/28.
+//
+
+import Foundation
+
+let NL = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NL[0]
+let L = NL[1]
+var pipe = Array(repeating: false, count: 1001)
+var cnt = 0
+let idx = readLine()!.split(separator: " ").map{Int(String($0))!}
+for i in idx{
+    pipe[i] = true
+}
+for hole in 1...1000{
+    if pipe[hole]{
+        cnt += 1
+        for tape in hole..<hole+L{
+            if tape > 1000{
+                break
+            }
+            pipe[tape] = false
+        }
+    }
+}
+print(cnt)
