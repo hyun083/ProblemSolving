75955359262ed4ee7355d53dbbadcd5e15ae638c
hyun083 <jh9501@gmail.com>
2022-03-12 21:28:04 +0900

    2022-03-12

--- 2437/2437_swift/2437_swift/main.swift
+++ 2437/2437_swift/2437_swift/main.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 let N = Int(readLine()!)!
-let weight = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
-var numbers = Set<Int>()
+let weights = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
+var number = 1
 
+for weight in weights {
+    if weight > number{
+        break
+    }
+    number += weight
+}
+print(number)
 
- 
