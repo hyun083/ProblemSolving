0e419753914b7901dbd481cd18d0a4db84e59ebe
hyun083 <jh9501@gmail.com>
2022-03-19 20:57:18 +0900

    2022-03-19

---
+++ 12790/12790_swift/12790_swift/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  12790_swift
+//
+//  Created by Hyun on 2022/03/19.
+//
+
+//import Foundation
+
+for _ in 0..<Int(readLine()!)!{
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let HP = input[0]+input[4]<1 ? 1:input[0]+input[4]
+    let MP = input[1]+input[5]<1 ? 1:input[1]+input[5]
+    let ATK = input[2]+input[6]<0 ? 0:input[2]+input[6]
+    let DEF = input[3]+input[7]
+    print(1*HP+5*MP+2*ATK+2*DEF)
+}
+
