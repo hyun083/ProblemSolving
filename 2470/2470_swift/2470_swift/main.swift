e9625afcd01ddb737bab017549e61cd28054e32e
hyun083 <jh9501@gmail.com>
2022-03-25 18:00:47 +0900

    2022-03-25

--- 2470/2470_swift/2470_swift/main.swift
+++ 2470/2470_swift/2470_swift/main.swift
@@ -9,23 +9,27 @@ import Foundation
 
 let N = Int(readLine()!)!
 let arr = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
-if arr[N-1]>0 && arr[0]<0{
-    var result = 1000000001
-    var idx1 = 0
-    var idx2 = 0
-    for i in 0..<N{
-        for k in 0..<N{
-            let number = abs(arr[i] + arr[k])
-            if result > number{
-                result = number
-                idx1 = i
-                idx2 = k
-            }
-        }
+var start = 0
+var end = N-1
+var result = Int.max
+var idx1 = 0
+var idx2 = 0
+
+while start < end{
+    let number = arr[start]+arr[end]
+    if result > abs(number){
+        result = abs(number)
+        idx1 = start
+        idx2 = end
     }
-    print("\(arr[idx1]) \(arr[idx2])")
-}else if arr[N-1] < 0 {
-    print("\(arr[N-2]) \(arr[N-1])")
-}else if arr[0] > 0{
-    print("\(arr[0]) \(arr[1])")
+    if number<0{
+        start += 1
+    }else if number>0{
+        end -= 1
+    }else{
+        idx1 = start
+        idx2 = end
+        break
+    }
 }
+print(arr[idx1], arr[idx2])
