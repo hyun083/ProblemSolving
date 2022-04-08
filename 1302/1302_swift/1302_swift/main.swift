57c721c10b8434d93c332749c08a793f911ba583
hyun <jh9501@gmail.com>
2022-04-08 22:51:44 +0900

    2022-04-08

--- 1302/1302_swift/1302_swift/main.swift
+++ 1302/1302_swift/1302_swift/main.swift
@@ -5,7 +5,21 @@
 //  Created by Hyun on 2022/04/08.
 //
 
-import Foundation
+//import Foundation
 
-print("Hello, World!")
-
+var arr = [String:Int]()
+for _ in 0..<Int(readLine()!)!{
+    let key = readLine()!
+    if arr[key] == nil{
+        arr[key] = 1
+    }else{
+        arr[key]! += 1
+    }
+}
+print(arr.sorted(by: {
+    if $0.value == $1.value{
+        return $0.key < $1.key
+    }else{
+        return $0.value > $1.value
+    }
+}).first!.key)
---
+++ 4358/4358_swift/4358_swift/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  4358_swift
+//
+//  Created by Hyun on 2022/04/08.
+//
+
+import Foundation
+
+var trees = [String:Double]()
+var total = 0.0
+while let name = readLine(){
+    total += 1
+    if trees[name] == nil{
+        trees[name] = 1
+    }else{
+        trees[name]! += 1
+    }
+}
+
+for tree in trees.sorted(by: {$0.key < $1.key}){
+    let ratio = (tree.value / total * 100)
+    print(tree.key, String(format: "%.4f", round(ratio*10000)/10000))
+}
+
