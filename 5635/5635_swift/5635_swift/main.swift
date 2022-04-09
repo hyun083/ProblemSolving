bcb70ae7eae1c62a8b607a2f5d45586cec99fe1d
hyun <jh9501@gmail.com>
2022-04-09 22:32:37 +0900

    2022-04-09

---
+++ 5635/5635_swift/5635_swift/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  5635_swift
+//
+//  Created by Hyun on 2022/04/09.
+//
+
+import Foundation
+
+struct Student{
+    var name:String
+    var dd:Int
+    var mm:Int
+    var yy:Int
+    init(_ n:String, _ d:Int, _ m:Int, _ y:Int){
+        name = n
+        dd = d
+        mm = m
+        yy = y
+    }
+}
+var students = [Student]()
+for _ in 0..<Int(readLine()!)!{
+    let info = readLine()!.split(separator: " ").map{String($0)}
+    students.append(Student(info[0], Int(info[1])!, Int(info[2])!, Int(info[3])!))
+}
+students.sort(by: {
+    if $0.yy == $1.yy{
+        if $0.mm == $1.mm{
+            return $0.dd < $0.dd
+        }else{
+            return $0.mm < $1.mm
+        }
+    }else{
+        return $0.yy < $1.yy
+    }
+})
+print(students.last!.name)
+print(students.first!.name)
