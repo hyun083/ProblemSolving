8eca3301394df28eef49a3b82747561e2dddb6eb
hyun <jh9501@gmail.com>
2022-04-14 01:04:21 +0900

    2022-04-14

--- 2002/2002_swift/2002_swift/main.swift
+++ 2002/2002_swift/2002_swift/main.swift
@@ -10,20 +10,22 @@ import Foundation
 let N = Int(readLine()!)!
 var cars = [String:Int]()
 var idx = 0
-var cnt = 0
-var temp = [Int](0..<N)
-
+var over = [Int]()
 
 for i in 0..<N{
     cars[readLine()!] = i
 }
 
 for _ in 0..<N{
-    if cars[readLine()!]! == idx{
-        
+    while over.contains(idx){
+        idx+=1
+    }
+    let car = cars[readLine()!]!
+    if car == idx{
+        idx += 1
     }else{
-        
+        over.append(car)
     }
 }
-print(cnt)
+print(over.count)
 
