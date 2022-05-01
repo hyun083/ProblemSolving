32e2d543970c2e4fdc207384ee6f448448202b0d
hyun <jh9501@gmail.com>
2022-05-01 23:14:54 +0900

    2022-05-01

---
+++ 16430/16430_swift/16430_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  16430_swift
+//
+//  Created by Hyun on 2022/05/01.
+//
+
+import Foundation
+
+let AB = readLine()!.split(separator: " ").map{Int(String($0))!}
+print(AB[1]-AB[0],AB[1])
+
