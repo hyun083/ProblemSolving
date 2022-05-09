e0d8f75b40fa82d92dbde8c35c2e5bae3df25fa0
hyun <jh9501@gmail.com>
2022-05-09 21:33:45 +0900

    2022-05-09

--- 17496/17496_swift/17496_swift/main.swift
+++ 17496/17496_swift/17496_swift/main.swift
@@ -5,7 +5,12 @@
 //  Created by Hyun on 2022/05/08.
 //
 
-import Foundation
+//import Foundation
 
-print("Hello, World!")
+let star = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = star[0] - 1
+let t = star[1]
+let c = star[2]
+let p = star[3]
+print((n/t)*c*p)
 
