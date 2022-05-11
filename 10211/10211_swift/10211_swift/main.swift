eaf2fcf790a0e56f2e35a80e6720b57b318ec35f
hyun <jh9501@gmail.com>
2022-05-11 23:42:20 +0900

    2022-05-11

---
+++ 10211/10211_swift/10211_swift/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  10211_swift
+//
+//  Created by Hyun on 2022/05/11.
+//
+
+import Foundation
+
+//for _ in 0..<Int(readLine()!)!{
+//    let n = Int(readLine()!)!
+//    let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+//    let result = -1000001
+//    var dp = Array(repeating: Array(repeating: 0, count: n), count: n)
+//
+//    for i in 0..<n{
+//        for k in 0..<n-i{
+//            var temp = 0
+//            for r in i..<k{
+//
+//            }
+//        }
+//    }
+//}
+
+
