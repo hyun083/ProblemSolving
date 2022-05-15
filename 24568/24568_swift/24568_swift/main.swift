a2b0353140f8f315a588d0d8abc3d6f60f2d4a3f
hyun <jh9501@gmail.com>
2022-05-15 23:16:25 +0900

    2022-05-15

---
+++ 24568/24568_swift/24568_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  24568_swift
+//
+//  Created by Hyun on 2022/05/15.
+//
+
+//import Foundation
+
+let r = Int(readLine()!)! * 8
+let s = Int(readLine()!)! * 3
+print((r+s)-28)
