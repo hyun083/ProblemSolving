ef4b0b73a2aba4f4f5da288cfcde132ebbdbceff
hyun <jh9501@gmail.com>
2022-05-22 20:29:43 +0900

    2022-05-22

---
+++ 13277/13277_python/main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    numbers = input().split(" ")
+    print(int(numbers[0]) * int(numbers[1]))
--- 2480/2480_swift/2480_swift/main.swift
+++ 2480/2480_swift/2480_swift/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+let numbers = readLine()!.split(separator: " ").map{Int(String($0))!}
+var result = [Int:Int]()
+for number in numbers {
+    if result[number] == nil{
+        result[number] = 1
+    }else{
+        result[number]! += 1
+    }
+}
+let temp = result.sorted(by: {
+    if $0.value == $1.value{
+        return $0.key > $1.key
+    }else{
+        return $0.value > $1.value
+    }
+})
 
+if temp.first!.value == 3{
+    print(temp.first!.key*1000+10000)
+}else if temp.first!.value == 2{
+    print(temp.first!.key*100+1000)
+}else{
+    print(temp.first!.key*100)
+}
