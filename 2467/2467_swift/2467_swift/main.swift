9130cead4d5d834aec1d7c57f846c122ce8ad9a1
hyun <jh9501@gmail.com>
2022-05-24 02:15:21 +0900

    2022-05-24

--- 2467/2467_swift/2467_swift/main.swift
+++ 2467/2467_swift/2467_swift/main.swift
@@ -8,6 +8,27 @@
 import Foundation
 
 let n = Int(readLine()!)!
-let arr = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
 
+var left = 0
+var right = n-1
+var temp = 2000000001
+var str = ""
+while left < right{
+    if arr[left] + arr[right] == 0{
+        str = "\(arr[left]) \(arr[right])"
+        break
+    }
+    if abs(arr[left] + arr[right]) < temp{
+        temp = abs(arr[left] + arr[right])
+        str = "\(arr[left]) \(arr[right])"
+    }
+    
+    if arr[left] + arr[right] < 0 {
+        left += 1
+    }else{
+        right -= 1
+    }
+}
+print(str)
 
