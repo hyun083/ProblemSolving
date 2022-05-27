7bab9cf4f39af89d4f32b1cd1c139ccccc6d0830
hyun <jh9501@gmail.com>
2022-05-27 17:12:04 +0900

    2022-05-27

---
+++ 11728/11728_swift/11728_swift/main.swift
@@ -0,0 +1,95 @@
+//
+//  main.swift
+//  11728_swift
+//
+//  Created by Hyun on 2022/05/26.
+//
+
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+let fio = FileIO()
+
+let n = fio.readInt()
+let m = fio.readInt()
+
+var arr = [Int]()
+var arr2 = [Int]()
+for _ in 0..<n{
+    arr.append(fio.readInt())
+}
+for _ in 0..<m{
+    arr2.append(fio.readInt())
+}
+arr += arr2.reversed()
+
+var start = 0
+var end = n+m-1
+var str = ""
+
+while start <= end{
+    if arr[start] < arr[end]{
+        str.append("\(arr[start]) ")
+        start += 1
+    }else{
+        str.append("\(arr[end]) ")
+        end -= 1
+    }
+}
+print(str)
