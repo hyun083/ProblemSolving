109438bc7660ad3d7dbf4feba68727e7bf37eece
hyun <jh9501@gmail.com>
2022-06-01 21:58:20 +0900

    2022-06-01

---
+++ 1806/1806_swift/1806_swift/main.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  1806_swift
+//
+//  Created by Hyun on 2022/06/01.
+//
+
+import Foundation
+
+let ns = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = ns[0]
+let s = ns[1]
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+var len = n+1
+var start = 0
+var end = 0
+var sum = arr[0]
+
+while start<=end && end<n{
+    if sum<s{
+        end += 1
+        if end == n{
+            break
+        }
+        sum += arr[end]
+    }else{
+        len = min(len, end - start + 1)
+        sum -= arr[start]
+        start += 1
+    }
+}
+if len == n+1{
+    print(0)
+}else{
+    print(len)
+}
+
