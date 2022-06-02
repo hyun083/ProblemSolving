d6d140c8f30f27f768886d8168c7ef1fcbb37814
hyun <jh9501@gmail.com>
2022-06-02 22:39:15 +0900

    2022-06-02

---
+++ 14921/14921_swift/14921_swift/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  14921_swift
+//
+//  Created by Hyun on 2022/06/02.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var left = 0
+var right = n-1
+var result = 200000001
+while left < right{
+    let sum = arr[left]+arr[right]
+    if sum == 0{
+        result = 0
+        break
+    }
+    if sum<0{
+        left += 1
+    }
+    if sum>0{
+        right -= 1
+    }
+    result = abs(result)<abs(sum) ? result:sum
+}
+print(result)
