230a5ddd00a115bb5c392d2c74d721d3697ad161
hyun <jh9501@gmail.com>
2022-06-04 21:11:45 +0900

    2022-06-04

---
+++ 11265/11265_swift/11265_swift/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  11265_swift
+//
+//  Created by Hyun on 2022/06/04.
+//
+
+import Foundation
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0]
+let m = nm[1]
+var arr = [[Int]]()
+for _ in 0..<n{
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+for k in 0..<n{
+    for u in 0..<n{
+        for v in 0..<n{
+            if u==v{continue}
+            arr[u][v] = min(arr[u][v], arr[u][k]+arr[k][v])
+        }
+    }
+}
+for _ in 0..<m{
+    let line = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let u = line[0]-1
+    let v = line[1]-1
+    let c = line[2]
+    if arr[u][v] <= c{
+        print("Enjoy other party")
+    }else{
+        print("Stay here")
+    }
+}
+
