79e31af577f08f2f24168313339f9934b9aa11dc
hyun <jh9501@gmail.com>
2022-06-28 17:00:58 +0900

    2022-06-28

--- 6443/6443_swift/6443_swift/main.swift
+++ 6443/6443_swift/6443_swift/main.swift
@@ -6,89 +6,33 @@
 //
 
 import Foundation
-//by Rhyno
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
 
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-
-        return buffer[index]
-    }
-
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-
-        while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-
-        return sum * (isPositive ? 1:-1)
-    }
-
-    @inline(__always) func readString() -> String {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return Array(buffer[beginIndex..<(index-1)])
-    }
-}
-let fio = FileIO()
-let T = fio.readInt()
-for _ in 0..<T{
-    let arr = fio.readString().map{String($0)}
+for _ in 0..<Int(readLine()!)!{
+    let arr = readLine()!
     let n = arr.count
-    var visited = Array(repeating: false, count: n)
-    var ans = Set<String>()
+    var visited = Array(repeating: 0, count: 26)
     var str = ""
+    for a in arr{
+        let idx = Int(a.asciiValue!-97)
+        visited[idx] += 1
+    }
     func btk(cnt:Int ,curr:Int){
         if cnt == n{
-            ans.insert(str)
+            if str.count == n{
+                print(str)
+            }
             return
         }
-        for i in 0..<n{
-            if !visited[i]{
-                visited[i] = true
-                str.append(arr[i])
+        for i in 0..<26{
+            if visited[i] > 0{
+                visited[i] -= 1
+                str.append(String(UnicodeScalar(UInt8(i+97))))
                 btk(cnt: cnt+1, curr: i)
-                visited[i] = false
+                visited[i] += 1
                 str.removeLast()
             }
         }
     }
     btk(cnt: 0, curr: 0)
-    let temp = ans.sorted(by: <).joined(separator: "\n")
-    print(temp)
 }
 
