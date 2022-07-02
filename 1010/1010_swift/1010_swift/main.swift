65fb7fc11f832d2ef4ea8dfd5ae08a314e5f7a4c
hyun <jh9501@gmail.com>
2022-07-02 17:48:30 +0900

    2022-07-02

--- 1010/1010_swift/1010_swift/main.swift
+++ 1010/1010_swift/1010_swift/main.swift
@@ -5,42 +5,20 @@
 //  Created by Hyun on 2022/04/02.
 //
 
-//import Foundation
+import Foundation
 
-//for _ in 0..<Int(readLine()!)!{
-//    let MN = readLine()!.split(separator: " ").map{Int(String($0))!}
-//    let m = MN[0]
-//    let n = MN[1]
-//    var temp = 1
-//    var f = 1
-//    for i in stride(from: n, to: n-m, by: -1){
-//        temp *= i
-//        temp /= f
-//        f += 1
-//    }
-//    print(temp)
-//}
-func solution(_ stones:[Int], _ k:Int) -> Int {
-    var ans = 0
-    var stones = stones
-    func check() -> Bool{
-        let check = Array(repeating:0, count:k)
-        for i in 0...stones.count-k{
-            if Array(stones[i..<i+k]) == check{return false}
-        }
-        return true
+for _ in 0..<Int(readLine()!)!{
+    let MN = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let m = MN[0]
+    let n = MN[1]
+    var temp = 1
+    var f = 1
+    for i in stride(from: n, to: n-m, by: -1){
+        temp *= i
+        temp /= f
+        f += 1
     }
-    while check(){
-        for i in 0..<stones.count{
-            if stones[i] <= 0 {
-                stones[i] = 0
-            }else{
-                stones[i] -= 1
-            }
-        }
-        print(stones)
-        ans += 1
-    }
-    return ans
+    print(temp)
 }
-print(solution([0, 0, 1, 3, 2, 1, 4, 2, 5, 1], 3))
+
+
---
+++ 1478/1478_swift/1478_swift/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  1478_swift
+//
+//  Created by Hyun on 2022/07/02.
+//
+
+import Foundation
+
+var esm = readLine()!.split(separator: " ").map{Int(String($0))!}
+var cnt = 1
+while esm != [1,1,1]{
+    cnt += 1
+    esm[0] -= 1
+    esm[1] -= 1
+    esm[2] -= 1
+    if esm[0] == 0{ esm[0] = 15 }
+    if esm[1] == 0{ esm[1] = 28 }
+    if esm[2] == 0{ esm[2] = 19 }
+}
+print(cnt)
