30b11368e95e3bbdb08adbd77a1e09ff08e27c00
hyun <jh9501@gmail.com>
2022-07-04 23:46:05 +0900

    2022-07-04

---
+++ 1977/1977_swift/1977_swift/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  1977_swift
+//
+//  Created by Hyun on 2022/07/04.
+//
+
+import Foundation
+
+let m = Int(readLine()!)!
+let n = Int(readLine()!)!
+var sum = 0
+var myMin = 0
+for i in stride(from: 100, to: 0, by: -1){
+    let number = Int(NSDecimalNumber(decimal:pow(Decimal(i), 2)))
+    if m<=number && number<=n{
+        sum += number
+        myMin = number
+    }
+}
+if sum == 0 && myMin==0{
+    print(-1)
+}else{
+    print(sum)
+    print(myMin)
+}
