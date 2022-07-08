d4c2c1f9c15a78c9cd9d5782f3b3d5d2f9a3fafb
hyun <jh9501@gmail.com>
2022-07-08 18:45:32 +0900

    2022-07-08

--- 2439/2439_swift/2439_swift/main.swift
+++ 2439/2439_swift/2439_swift/main.swift
@@ -5,7 +5,17 @@
 //  Created by Hyun on 2022/07/07.
 //
 
-import Foundation
+//import Foundation
 
-print("Hello, World!")
+let n = Int(readLine()!)!
+for i in 1...n{
+    var stars = ""
+    for _ in 0..<n-i{
+        stars += " "
+    }
+    for _ in 0..<i{
+        stars += "*"
+    }
+    print(stars)
+}
 
---
+++ 8393/8393_swift/8393_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  8393_swift
+//
+//  Created by Hyun on 2022/07/08.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+print(n*(n+1)/2)
+
