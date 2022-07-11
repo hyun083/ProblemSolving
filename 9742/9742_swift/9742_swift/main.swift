11ba57c5677cbf6a62fe3de06432abe43129a878
hyun <jh9501@gmail.com>
2022-07-11 21:27:36 +0900

    2022-07-11

--- 9742/9742_swift/9742_swift/main.swift
+++ 9742/9742_swift/9742_swift/main.swift
@@ -7,18 +7,40 @@
 
 import Foundation
 
-while let input = readLine()?.split(separator: " ").map{String($0)}{
-    let txt = input[0]
-    let num = Int(input[1])!
-    var fact = 1
-    for i in 1...txt.count{
-        fact *= i
-    }
-    if fact < num{
-        print("No permutation")
-    }else{
-        
-    }
+while let line = readLine(){
+    let input = line.split(separator: " ").map{String($0)}
+    let arr = input[0].map{String($0)}.sorted(by: <)
+    let idx = Int(input[1])!-1
+    var visited = Array(repeating: false, count: arr.count)
+    var result = [String]()
+    var ans = [String]()
     
+    func btk(cnt:Int){
+        if result.count==arr.count{
+            ans.append(result.joined())
+            return
+        }
+        for i in 0..<arr.count{
+            if !visited[i]{
+                visited[i] = true
+                result.append(arr[i])
+                btk(cnt: cnt+1)
+                visited[i] = false
+                result.removeLast()
+            }
+        }
+    }
+    for i in 0..<arr.count{
+        visited[i] = true
+        result.append(arr[i])
+        btk(cnt: 0)
+        visited[i] = false
+        result.removeLast()
+    }
+    if ans.count < idx{
+        print(input[0],input[1],"=","No permutation")
+    }else{
+        print(input[0],input[1],"=",ans[idx])
+    }
 }
 
