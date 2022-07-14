b76a17bc844a482267c4dcbe8d842c80f769b356
hyun <jh9501@gmail.com>
2022-07-15 00:11:24 +0900

    2022-07-15

--- 19949/19949_swift/19949_swift/main.swift
+++ 19949/19949_swift/19949_swift/main.swift
@@ -11,49 +11,35 @@ let targets = readLine()!.split(separator: " ").map{Int(String($0))!}
 var arr = [1,2,3,4,5]
 var pick = [Int]()
 var ans = 0
+var score = 0
 
-func btk(cnt:Int, flag:Bool, score:Int){
+func btk(cnt:Int){
     if cnt == 10{
-        if score > 4{
-            ans += 1
+        var score = 0
+        for i in 0..<10{
+            if pick[i] == targets[i] {score += 1}
         }
+        if score >= 5{ans += 1}
         return
     }
     for i in 0..<5{
-        if !(flag && arr[i] == pick.last!){
-            if arr[i] == pick.last!{
-                if targets[i] == arr[i]{
-                    pick.append(arr[i])
-                    btk(cnt: cnt+1, flag: true, score: score+1)
-                    pick.removeLast()
-                }else{
-                    pick.append(arr[i])
-                    btk(cnt: cnt+1, flag: true, score: score)
-                    pick.removeLast()
-                }
-            }else{
-                if targets[i] == arr[i]{
-                    pick.append(arr[i])
-                    btk(cnt: cnt+1, flag: false, score: score+1)
-                    pick.removeLast()
-                }else{
-                    pick.append(arr[i])
-                    btk(cnt: cnt+1, flag: false, score: score)
-                    pick.removeLast()
-                }
+        if cnt > 1{
+            if pick[cnt-1]==arr[i] && pick[cnt-2]==arr[i]{
+                continue
             }
+            pick.append(arr[i])
+            btk(cnt: cnt+1)
+            pick.removeLast()
+        }else{
+            pick.append(arr[i])
+            btk(cnt: cnt+1)
+            pick.removeLast()
         }
     }
 }
 for i in 0..<5{
-    if targets[i] == arr[i]{
-        pick.append(arr[i])
-        btk(cnt: 1, flag: false, score: 1)
-        pick.removeLast()
-    }else{
-        pick.append(arr[i])
-        btk(cnt: 1, flag: false, score: 0)
-        pick.removeLast()
-    }
+    pick.append(arr[i])
+    btk(cnt: 1)
+    pick.removeLast()
 }
 print(ans)
