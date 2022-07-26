ae2fe60fc3946d805246974afbd6fc447271aa8c
hyun <jh9501@gmail.com>
2022-07-26 17:14:22 +0900

    2022-07-26

--- 23284/23284_swift/23284_swift/main.swift
+++ 23284/23284_swift/23284_swift/main.swift
@@ -16,10 +16,37 @@ var ans = [String]()
 
 func btk(cnt:Int, limit:Int, prev:Int){
     if cnt == n{
-        
+        ans.append(res.joined(separator: " "))
         return
     }
-    
+    for i in 1...n{
+        if !visited[i]{
+            if prev<i{
+                if limit+1<=i{
+                    visited[i] = true
+                    res.append(numbers[i])
+                    btk(cnt: cnt+1, limit: max(limit, i), prev: i)
+                    res.removeLast()
+                    visited[i] = false
+                }
+            }else{
+                var flag = true
+                for i in i+1...prev{
+                    if !res.contains(String(i)){
+                        flag = false
+                        break
+                    }
+                }
+                if flag{
+                    visited[i] = true
+                    res.append(numbers[i])
+                    btk(cnt: cnt+1, limit: max(limit, i), prev: i)
+                    res.removeLast()
+                    visited[i] = false
+                }
+            }
+        }
+    }
 }
 btk(cnt: 0, limit: 0, prev: 0)
-
+print(ans.joined(separator: "\n"))
