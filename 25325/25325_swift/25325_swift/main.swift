355c70f4309fe14892017f138d8291239980d435
hyun <jh9501@gmail.com>
2022-07-27 23:47:26 +0900

    2022-07-27

---
+++ 25325/25325_swift/25325_swift/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  25325_swift
+//
+//  Created by Hyun on 2022/07/27.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var dic = [String:Int]()
+for name in readLine()!.split(separator: " ").map{String($0)}{
+    dic[name] = 0
+}
+for _ in 0..<n{
+    let names = readLine()!.split(separator: " ").map{String($0)}
+    for name in names{
+        dic[name]! += 1
+    }
+}
+for output in dic.sorted(by: {
+    if $0.value == $1.value{
+        return $0.key < $1.key
+    }else{
+        return $0.value > $1.value
+    }
+}){
+    print(output.key, output.value)
+}
