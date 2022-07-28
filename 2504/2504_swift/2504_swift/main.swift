fb3518c1fe720b5cb42a9206c3d90978d744578c
hyun <jh9501@gmail.com>
2022-07-29 02:02:08 +0900

    2022-07-29

--- 2504/2504_swift/2504_swift/main.swift
+++ 2504/2504_swift/2504_swift/main.swift
@@ -11,26 +11,5 @@ let arr = readLine()!.map{String($0)}
 var stack = [String]()
 var ans = 0
 
-for str in arr{
-    if str == "(" || str == "["{
-        stack.append(str)
-    }
-    if str == ")"{
-        if let top = stack.last{
-            if top == "("{
-                stack.removeLast()
-                if stack.isEmpty{
-                    ans += 2
-                }else if stack.last! == ""{
-                    
-                }
-            }
-        }else{
-            stack.append(str)
-        }
-    }
-    if str == "]"{
-        
-    }
-}
+
 
