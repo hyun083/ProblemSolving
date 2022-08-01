6c9b3901c9949bffe2f35bd65ef455e8d0963daf
hyun <jh9501@gmail.com>
2022-08-01 21:42:51 +0900

    2022-08-01

---
+++ 2742/2742_swift/2742_swift/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  2742_swift
+//
+//  Created by Hyun on 2022/08/01.
+//
+
+//import Foundation
+
+for number in stride(from: Int(readLine()!)!, through: 1, by: -1){
+    print(number)
+}
+
