0cf56fe8c502d24c4e3bbd1f4d918b51ffbffc95
hyun <jh9501@gmail.com>
2022-08-07 17:24:21 +0900

    2022-08-07

---
+++ 1380/1380_swift/1380_swift/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  1380_swift
+//
+//  Created by Hyun on 2022/08/07.
+//
+
+import Foundation
+var T = 1
+while let line = readLine(){
+    if line == "0"{
+        break
+    }
+    let n = Int(line)!
+    var girls = Array(repeating: String(), count: n)
+    var visited = Array(repeating: false, count: n)
+    for i in 0..<n{
+        girls[i] = readLine()!
+    }
+    for _ in 0..<2*n-1{
+        let earring = readLine()!.split(separator: " ").map{String($0)}
+        let number = Int(earring[0])! - 1
+        visited[number] = !visited[number]
+    }
+    for i in 0..<n{
+        if visited[i]{
+            print(T,girls[i])
+        }
+    }
+    T += 1
+}
+
