376054ce03975fb62189a6c42578f73b8f0caa05
hyun <jh9501@gmail.com>
2022-08-25 18:53:55 +0900

    2022-08-25
    1300 - binarySearch

---
+++ 1300/1300_swift/1300_swift/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  1300_swift
+//
+//  Created by Hyun on 2022/08/25.
+//
+
+import Foundation
+
+func cnt(of num:Int) -> Int{
+    var result = 0
+    for i in 1...n{
+        result += min(num/i, n)
+    }
+    return result
+}
+
+let n = Int(readLine()!)!
+let k = Int(readLine()!)!
+
+var start = 1
+var end = n*n
+
+while start <= end{
+    let mid = (start+end)/2
+    let cnt = cnt(of: mid)
+    if cnt >= k{
+        end = mid-1
+        continue
+    }else{
+        start = mid+1
+        continue
+    }
+}
+print(start)
