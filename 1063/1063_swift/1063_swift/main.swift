5213a049a098e75cbe7880dfec50270f9798e94b
hyun <jh9501@gmail.com>
2022-08-27 23:46:45 +0900

    2022-08-27
    1233 - brute force

---
+++ 1063/1063_swift/1063_swift/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  1063_swift
+//
+//  Created by Hyun on 2022/08/27.
+//
+
+import Foundation
+
+let line = readLine()!.split(separator: " ").map{String($0)}
+var king:[Int]
+var dol:[Int]
+
+
+
+
+let n = Int(line[2])!
+
+for _ in 0..<n{
+    let cmd = readLine()!
+    
+}
+
