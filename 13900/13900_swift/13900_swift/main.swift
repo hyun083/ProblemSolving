3c3e219846445a217976c3ca4c87a3014552e847
hyun <jh9501@gmail.com>
2022-09-09 22:49:12 +0900

    2022-09-09
    13900 - prefix_sum

---
+++ 13900/13900_swift/13900_swift/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  13900_swift
+//
+//  Created by Hyun on 2022/09/09.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var ans = 0
+var temp = 0
+let arr = [0] + readLine()!.split(separator: " ").map{Int($0)!}
+let psum = arr.map{num -> Int in
+    temp += num
+    return temp
+}
+
+for i in 1..<n{
+    ans += arr[i] * (psum[n]-psum[i])
+}
+print(ans)
