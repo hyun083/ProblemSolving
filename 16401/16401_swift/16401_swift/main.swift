3292bcfa600dfa4df855809ed8ed0b37febdcacb
hyun <jh9501@gmail.com>
2022-10-10 21:51:51 +0900

    binary_search
    16401 - binary_search

---
+++ 16401/16401_swift/16401_swift/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  16401_swift
+//
+//  Created by Hyun on 2022/10/10.
+//
+
+import Foundation
+
+let mn = readLine()!.split(separator: " ").map{Int(String($0))!}
+let m = mn[0]   //조카들의 수
+let n = mn[1]   //과자의 수
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var start = 1
+var end = arr.max()!
+var ans = 0
+while start <= end{
+    var cnt = 0
+    let mid = (start+end)/2
+    for length in arr{
+        cnt += length/mid
+    }
+    if cnt>=m{
+        ans = max(ans, mid)
+        start = mid+1
+    }else if cnt<m{
+        end = mid-1
+    }
+}
+print(ans)
