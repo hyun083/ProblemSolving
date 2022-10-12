dc5c6a27471b31c21944ed9afbaf9f4d1e794f38
hyun <jh9501@gmail.com>
2022-10-12 18:17:19 +0900

    BFS
    17071 - BFS

--- 17071/17071_swift/17071_swift/main.swift
+++ 17071/17071_swift/17071_swift/main.swift
@@ -12,40 +12,36 @@ let n = nk[0]
 var k = nk[1]
 var ans = -1
 
-var map = Array(repeating: Int.max, count: 500001)
-map[n] = 0
+var map = Array(repeating: Array(repeating: false, count: 2), count: 500001)
+map[n][0] = true
+var time = 0
+
 func bfs(){
     var q = [n]
     var dq = [Int]()
     while !q.isEmpty{
+        k += time
         dq = q.reversed()
         q.removeAll()
+        if k<0 || k>500000{return}
+        if map[k][time%2]{
+            ans = time
+            return
+        }
         for _ in 0..<dq.count{
             let curr = dq.removeLast()
-            let time = map[curr]
             let next = [curr-1,curr+1,curr*2]
             
             for next in next{
                 if next<0 || next>=500001{continue}
-                if map[next] > time+1{
-                    map[next] = time+1
+                if !map[next][(time+1)%2]{
+                    map[next][(time+1)%2] = true
                     q.append(next)
                 }
             }
         }
+        time += 1
     }
 }
 bfs()
-var sec = 0
-while k < 500001{
-    if map[k]<=sec && map[k]%2==0 && sec%2==0{
-        ans = sec
-        break
-    }else if map[k]<=sec && map[k]%2==1 && sec%2==1{
-        ans = sec
-        break
-    }
-    sec += 1
-    k += sec
-}
 print(ans)
