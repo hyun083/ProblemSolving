7822ddae278ebd93111966b9a6ecfa850ae7c97e
hyun <jh9501@gmail.com>
2022-10-23 22:19:33 +0900

    DP
    14002 - DP

---
+++ 14002/14002_swift/14002_swift/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  14002_swift
+//
+//  Created by Hyun on 2022/10/23.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var dp = Array(repeating: 1, count: n)
+var ans = Array(repeating: [String](), count: n)
+
+for last in 0..<n{
+    ans[last] = ["\(arr[last])"]
+    for number in 0..<last{
+        if arr[number] < arr[last]{
+            if dp[last] < dp[number]+1{
+                dp[last] = dp[number]+1
+                ans[last] = ans[number] + ["\(arr[last])"]
+            }
+        }
+    }
+}
+let idx = dp.firstIndex(of: dp.max()!)!
+print(dp[idx])
+print(ans[idx].joined(separator: " "))
