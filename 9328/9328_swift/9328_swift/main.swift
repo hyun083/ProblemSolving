23b3ff2a350269ef48a2cc86a814c75d2cefb564
hyun <jh9501@gmail.com>
2022-10-25 17:39:59 +0900

    BFS
    9328 - BFS

--- 9328/9328_swift/9328_swift/main.swift
+++ 9328/9328_swift/9328_swift/main.swift
@@ -13,22 +13,18 @@ for testcase in 0..<t{
     let wh = readLine()!.split(separator: " ").map{Int(String($0))!}
     let h = wh[0]
     let w = wh[1]
-    var map = Array(repeating: Array(repeating: "", count: w), count: h)
-    var visited = Array(repeating: Array(repeating: false, count: w), count: h)
-    var door = Array(repeating: [[Int]](), count: 26)
-    var keys = Set<String>()
+    var map = Array(repeating: Array(repeating: ".", count: w+2), count: h+2)
+    var visited = Array(repeating: Array(repeating: false, count: w+2), count: h+2)
+    var doors = Array(repeating: [[Int]](), count: 26)
+    var keys = Array(repeating: false, count: 26)
     var cnt = 0
-    for i in 0..<h{
-        let line = readLine()!.map{String($0)}
-        for k in 0..<w{
+    
+    for i in 1...h{
+        let line = ["."]+readLine()!.map{String($0)}+["."]
+        for k in 0..<w+2{
             map[i][k] = line[k]
             if map[i][k] == "*"{
                 visited[i][k] = true
-            }else{
-                let idx = Int(map[i][k].first!.asciiValue!) - 65
-                if 0<=idx && idx<26{
-                    door[idx].append([i,k])
-                }
             }
         }
     }
@@ -36,9 +32,8 @@ for testcase in 0..<t{
     
     for key in key{
         if key == "0" { continue }
-        if !keys.contains(key){
-            keys.insert(key)
-        }
+        let idx = Int(key.first!.asciiValue!)-97
+        keys[idx] = true
     }
     
     let dx = [-1,1,0,0]
@@ -57,109 +52,61 @@ for testcase in 0..<t{
                 for i in 0..<4{
                     let nx = x+dx[i]
                     let ny = y+dy[i]
-                    if nx<0 || nx>=h || ny<0 || ny>=w || map[nx][ny]=="*"{ continue }
+                    if nx<0 || nx>=h+2 || ny<0 || ny>=w+2 || map[nx][ny]=="*"{ continue }
+                    
+                    //방문하지 않은 빈공간이라면
                     if map[nx][ny]=="." && !visited[nx][ny]{
                         visited[nx][ny] = true
                         q.append([nx,ny])
+                        continue
                     }
-                    let value = Int(map[nx][ny].first!.asciiValue!)
-                    if (65<=value && value<91) && !visited[nx][ny]{
-                        let key = map[nx][ny].lowercased()
-                        if keys.contains(key){
-                            visited[nx][ny] = true
-                            q.append([nx,ny])
-                        }
-                    }else if (97<=value && value<123) && !visited[nx][ny]{
-                        visited[nx][ny] = true
-                        q.append([nx,ny])
-                        keys.insert(map[nx][ny])
-                        //문 확인
-                        let idx = value-32-65
-                        var temp = door[idx]
-                        door[idx].removeAll()
-                        for _ in 0..<temp.count{
-                            let next = temp.removeLast()
-                            var check = ""
-                            for i in 0..<4{
-                                let nnx = next[0] + dx[i]
-                                let nny = next[1] + dy[i]
-                                if nnx<0 || nnx>=h || nny<0 || nny>=w{ continue }
-                                check += map[nnx][nny]
-                                if map[nnx][nny] != "*" && visited[nnx][nny]{
-                                    visited[next[0]][next[1]] = true
-                                    q.append([next[0],next[1]])
-                                    break
-                                }
-                            }
-                            if !(check == "*" || check == "**" || check == "***" || check == "****"){
-                                door[idx].append([next[0],next[1]])
-                            }
-                        }
-                    }else if map[nx][ny]=="$" && !visited[nx][ny]{
+                    
+                    //방문하지 않은 문서라면
+                    if map[nx][ny]=="$" && !visited[nx][ny]{
                         visited[nx][ny] = true
                         cnt += 1
                         q.append([nx,ny])
+                        continue
+                    }
+                    
+                    let idx = Int(map[nx][ny].first!.asciiValue!)
+                    //방문하지 않은 문이라면
+                    if (65<=idx && idx<=90) && !visited[nx][ny]{
+                        if keys[idx-65]{
+                            visited[nx][ny] = true
+                            q.append([nx,ny])
+                        }else{
+                            doors[idx-65].append([nx,ny])
+                        }
+                        continue
+                    }
+                    
+                    //방문하지 않은 열쇠하면
+                    if (97<=idx && idx<=122) && !visited[nx][ny]{
+                        visited[nx][ny] = true
+                        q.append([nx,ny])
+                        //열쇠 추가
+                        if !keys[idx-97] {
+                            keys[idx-97] = true
+                        }
+                        
+                        //해당 열쇠를 얻음으로 인해 기존에 열지못한 문 탐색
+                        for door in doors[idx-97]{
+                            let nnx = door[0]
+                            let nny = door[1]
+                            visited[nnx][nny] = true
+                            q.append([nnx,nny])
+                        }
+                        doors[idx-97].removeAll()
                     }
                 }
             }
         }
     }
-    for x in 0..<h{
-        var y = [Int]()
-        if x==0 || x==h-1{
-            y = [Int](0..<w)
-        }else{
-            y = [0,w-1]
-        }
-        for y in y{
-            if map[x][y] == "*" { continue }
-            if map[x][y]=="." && !visited[x][y]{
-                visited[x][y] = true
-                bfs(a: x, b: y)
-            }else if map[x][y]=="$" && !visited[x][y]{
-                visited[x][y] = true
-                cnt += 1
-                bfs(a: x, b: y)
-            }else{
-                let value = Int(map[x][y].first!.asciiValue!)
-                if (97<=value && value<122) && !visited[x][y]{
-                    visited[x][y] = true
-                    let idx = value-32-65
-                    var temp = door[idx]
-                    door[idx].removeAll()
-                    for _ in 0..<temp.count{
-                        let next = temp.removeLast()
-                        var check = ""
-                        for i in 0..<4{
-                            let nnx = next[0] + dx[i]
-                            let nny = next[1] + dy[i]
-                            if nnx<0 || nnx>=h || nny<0 || nny>=w{ continue }
-                            check += map[nnx][nny]
-                            if map[nnx][nny] != "*" && visited[nnx][nny]{
-                                visited[next[0]][next[1]] = true
-                                bfs(a: next[0], b: next[1])
-                                break
-                            }
-                        }
-                        if !(check == "*" || check == "**" || check == "***" || check == "****"){
-                            door[idx].append([next[0],next[1]])
-                        }
-                    }
-                    bfs(a: x, b: y)
-                    keys.insert(map[x][y])
-                }else if 65<=value && value<91 && !visited[x][y]{
-                    let key = map[x][y].lowercased()
-                    if keys.contains(key){
-                        visited[x][y] = true
-                        bfs(a: x, b: y)
-                    }
-                }
-            }
-        }
-    }
+    
+    //경로탐색 시작
+    bfs(a: 0, b: 0)
     ans[testcase] = cnt
     print(cnt)
 }
-
-
 
