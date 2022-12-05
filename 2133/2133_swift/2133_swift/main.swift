9b49d1d769df639a9097e61a9955505fed7db0c0
hyun <jh9501@gmail.com>
2022-12-05 18:13:24 +0900

    DP
    2133 - DP

---
+++ 2133/2133_swift/2133_swift/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  2133_swift
+//
+//  Created by Hyun on 2022/12/05.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+var map = Array(repeating: 0, count: 31)
+map[0] = 1
+map[2] = 3
+
+for n in stride(from: 4, through: 30, by: +2){
+    map[n] = map[n-2] * map[2]
+    for k in stride(from: n-4, through: 0, by: -2){
+        map[n] += map[k]*2
+    }
+}
+
+print(map[N])
