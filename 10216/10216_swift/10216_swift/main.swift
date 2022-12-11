afa629af2ef8fe6b7a2da155e8feb903fd77fcae
hyun <jh9501@gmail.com>
2022-12-12 00:16:35 +0900

    unionFind
    10216 - unionFind

--- 10216/10216_swift/10216_swift/main.swift
+++ 10216/10216_swift/10216_swift/main.swift
@@ -7,114 +7,50 @@
 
 import Foundation
 
-//by Rhyno
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
-
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-
-        return buffer[index]
-    }
-
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-
-        while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-
-        return sum * (isPositive ? 1:-1)
-    }
-
-    @inline(__always) func readString() -> String {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return Array(buffer[beginIndex..<(index-1)])
-    }
+func dist(x1:Double, x2:Double, y1:Double, y2:Double) -> Double{
+    return sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)))
 }
 
-struct Camp{
-    let x:Int
-    let y:Int
-    let r:Int
-}
-
-var fio = FileIO()
-let T = fio.readInt()
 var output = ""
-for t in 0..<T{
-    let n = fio.readInt()
-    var ans = n
-    var arr = [Int](0..<n)
-    func root(Of n:Int)->Int{
-        if arr[n] == n {return n}
-        arr[n] = root(Of: arr[n])
+
+for _ in 0..<Int(readLine()!)!{
+    let n = Int(readLine()!)!
+    var arr = Array(repeating: -1, count: n)
+    var map = [(x:Double,y:Double,r:Double)]()
+    
+    func root(of n:Int) -> Int{
+        if arr[n]<0 {return n}
+        arr[n] = root(of: arr[n])
         return arr[n]
     }
+    
     func union(a:Int, b:Int){
-        let ra = root(Of: a)
-        let rb = root(Of: b)
-        arr[rb] = ra
+        let rootOfA = root(of: a)
+        let rootOfB = root(of: b)
+        if rootOfA == rootOfB{ return }
+        arr[rootOfB] = rootOfA
     }
-    var camps =  [Camp]()
-    for _ in 0..<n{
-        let x = fio.readInt()
-        let y = fio.readInt()
-        let r = fio.readInt()
-        camps.append(Camp(x: x, y: y, r: r))
-    }
-    for i in 0..<n-1{
-        for k in i+1..<n{
-            if root(Of: i) == root(Of: k){
-                continue
-            }
-            let a = camps[i]
-            let b = camps[k]
-            
-            let distance = pow(Decimal(a.x-b.x), 2) + pow(Decimal(a.y-b.y), 2)
-            let range = pow(Decimal(a.r+b.r), 2)
-            if distance <= range{
+    
+    for i in 0..<n{
+        let info = readLine()!.split(separator: " ").map{Double($0)!}
+        let x = info[0]
+        let y = info[1]
+        let r = info[2]
+        map.append((x,y,r))
+        
+        for k in 0..<i{
+            let target = map[k]
+            if target.r + r >= dist(x1: target.x, x2: x, y1: target.y, y2: y){
                 union(a: i, b: k)
-                ans -= 1
             }
         }
     }
-    if t == T-1{
-        output += "\(ans)"
-        break
+    
+    var ans = 0
+    for i in 0..<n{
+        if arr[i]<0{
+            ans += 1
+        }
     }
     output += "\(ans)\n"
 }
