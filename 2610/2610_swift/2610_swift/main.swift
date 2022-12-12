8775991f26e133133ff94085d7ed84948a0db519
hyun <jh9501@gmail.com>
2022-12-12 18:05:26 +0900

    unionFind
    20955 - unionFind

--- 2610/2610_swift/2610_swift/main.swift
+++ 2610/2610_swift/2610_swift/main.swift
@@ -9,12 +9,44 @@ import Foundation
 
 let n = Int(readLine()!)!
 let m = Int(readLine()!)!
-var map = Array(repeating: Array(repeating: 101, count: n), count: n)
+var arr = Array(repeating: -1, count: n)
+var info = [(a:Int,b:Int)]()
+var dist = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+func root(of a:Int)->Int{
+    if arr[a]<0 { return a }
+    arr[a] = root(of: arr[a])
+    return arr[a]
+}
+
+func union(a:Int, b:Int){
+    let rootA = root(of: a)
+    let rootB = root(of: b)
+    if rootA == rootB {return}
+    if arr[rootA] <= arr[rootB]{
+        arr[rootB] = rootA
+    }else{
+        arr[rootA] = rootB
+    }
+}
 
 for _ in 0..<m{
-    let people = readLine()!.split(separator: " ").map{Int(String($0))!}
-    let u = people[0]
-    let v = people[1]
-    map[u][v] = 1
+    let line = readLine()!.split(separator: " ").map{Int($0)!-1}
+    let u = line[0]
+    let v = line[1]
+    union(a: u, b: v)
+    dist[u][v] = 1
+    dist[v][u] = 1
+}
+
+for d in dist{
+    print(d)
+}
+for p in 0..<n{
+    for i in 0..<n{
+        for k in 0..<n{
+            dist[i][k] = max(dist[i][k], dist[i][p] + dist[p][k])
+        }
+    }
 }
 
