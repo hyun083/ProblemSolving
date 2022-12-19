812833982d56ecd9c8669ea7318ffc7e79cf18ce
hyun <jh9501@gmail.com>
2022-12-19 23:06:44 +0900

    unionFind, floydWarshall
    2610 - unionFind, floydWarshall

--- 2610/2610_swift/2610_swift/main.swift
+++ 2610/2610_swift/2610_swift/main.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
+let INF = Int.max
 let n = Int(readLine()!)!
 let m = Int(readLine()!)!
 var arr = Array(repeating: -1, count: n)
 var info = [(a:Int,b:Int)]()
-var dist = Array(repeating: Array(repeating: 0, count: n), count: n)
+var map = Array(repeating: Array(repeating: INF, count: n), count: n)
 
 func root(of a:Int)->Int{
     if arr[a]<0 { return a }
@@ -23,11 +24,34 @@ func union(a:Int, b:Int){
     let rootA = root(of: a)
     let rootB = root(of: b)
     if rootA == rootB {return}
-    if arr[rootA] <= arr[rootB]{
-        arr[rootB] = rootA
-    }else{
-        arr[rootA] = rootB
+    arr[rootB] = rootA
+}
+
+func dist(from a:Int) -> Int{
+    var visited = Array(repeating: false, count: n)
+    var q = [a]
+    var idx = 0
+    var myMin = INF
+    var ans = a
+    visited[a] = true
+    while idx<q.count{
+        let curr = q[idx]
+        var time = 0
+        for i in 0..<n{
+            if map[curr][i]==INF{continue}
+            time = max(time, map[curr][i])
+            if !visited[i]{
+                visited[i] = true
+                q.append(i)
+            }
+        }
+        if myMin>time{
+            myMin = time
+            ans = curr
+        }
+        idx += 1
     }
+    return ans+1
 }
 
 for _ in 0..<m{
@@ -35,18 +59,29 @@ for _ in 0..<m{
     let u = line[0]
     let v = line[1]
     union(a: u, b: v)
-    dist[u][v] = 1
-    dist[v][u] = 1
+    map[u][v] = 1
+    map[v][u] = 1
 }
 
-for d in dist{
-    print(d)
-}
 for p in 0..<n{
     for i in 0..<n{
         for k in 0..<n{
-            dist[i][k] = max(dist[i][k], dist[i][p] + dist[p][k])
+            if map[i][p]==INF || map[p][k]==INF || i==k{continue}
+            map[i][k] = min(map[i][k], map[i][p]+map[p][k])
         }
     }
 }
 
+var ans = [Int]()
+var cnt = 0
+for i in 0..<n{
+    if arr[i] < 0{
+        cnt += 1
+        ans.append(dist(from: i))
+    }
+}
+
+print(cnt)
+for ans in ans.sorted(by: <){
+    print(ans)
+}
