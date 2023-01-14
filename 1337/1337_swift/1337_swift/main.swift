a00d96505a74732630c0c75a5e3da567dd791c3a
hyun <jh9501@gmail.com>
2023-01-14 22:10:43 +0900

    brute_force
    1337 - brute_force

---
+++ 1337/1337_swift/1337_swift/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  1337_swift
+//
+//  Created by Hyun on 2023/01/14.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var map = [Int]()
+var ans = 0
+var cnt = 1
+
+for _ in 0..<n{
+    map.append(Int(readLine()!)!)
+}
+map.sort(by: <)
+
+for i in 0..<n{
+    cnt = 1
+    for k in i+1..<i+5{
+        if k>=n{ break }
+        if map[k]-map[i]<5{ cnt += 1}
+    }
+    ans = max(ans, cnt)
+}
+print(5-ans)
