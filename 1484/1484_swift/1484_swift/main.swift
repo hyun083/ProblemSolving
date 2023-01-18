d613b9a19c4d6e0cd6a0435257a52e71b9e787cc
hyun <jh9501@gmail.com>
2023-01-18 22:37:55 +0900

    two_pointer
    1484 - two_pointer

---
+++ 1484/1484_swift/1484_swift/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  1484_swift
+//
+//  Created by Hyun on 2023/01/18.
+//
+
+import Foundation
+
+var arr = Array(repeating: 0, count: 100001)
+for i in 0..<100001{ arr[i] = i*i }
+
+let g = Int(readLine()!)!
+var head = 1
+var tail = 2
+var ans = [Int]()
+
+while tail < 100001{
+    let gap = arr[tail] - arr[head]
+    if gap > g{
+        head += 1
+    }else{
+        tail += 1
+    }
+    if gap == g{
+        ans.append(tail-1)
+    }
+}
+
+if ans.isEmpty{
+    print(-1)
+}else{
+    for ans in ans{ print(ans) }
+}
