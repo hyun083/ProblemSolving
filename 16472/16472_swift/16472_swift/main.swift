a40e77b61f23eaca7f2b1348ca8995157b7f3754
hyun <jh9501@gmail.com>
2023-01-25 17:00:30 +0900

    two_pointer
    16472 - two_pointer

---
+++ 16472/16472_swift/16472_swift/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  16472_swift
+//
+//  Created by Hyun on 2023/01/25.
+//
+
+import Foundation
+
+
+let n = Int(readLine()!)!
+let arr = readLine()!.map{String($0)}
+
+var head = 0
+var tail = 0
+var ans = 0
+
+var dic = Dictionary<String,Int>()
+
+while tail<arr.count{
+    let str = arr[tail]
+    if dic.count >= n{
+        if dic[str] == nil{
+            if dic[arr[head]]! == 1{
+                dic.removeValue(forKey: arr[head])
+            }else{
+                dic[arr[head]]! -= 1
+            }
+            head += 1
+        }else{
+            dic[str]! += 1
+            tail += 1
+        }
+    }else{
+        if dic[str] == nil{
+            dic[str] = 1
+        }else{
+            dic[str]! += 1
+        }
+        tail += 1
+    }
+    if dic.count <= n{
+        ans = max(ans, tail - head)
+    }
+}
+print(ans)
