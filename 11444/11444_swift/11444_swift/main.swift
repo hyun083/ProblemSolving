afcb80df24d50b8bd3c3f7363643a673838cd993
hyun <jh9501@gmail.com>
2023-02-08 18:53:26 +0900

    math
    11444 - math

--- 11444/11444_swift/11444_swift/main.swift
+++ 11444/11444_swift/11444_swift/main.swift
@@ -7,26 +7,24 @@
 
 import Foundation
 
-let n = readLine()!
-let number = NSDecimalNumber(string: n)
-var arr = Dictionary<NSDecimalNumber,Int>()
+let n = Int(readLine()!)!
+var arr = Dictionary<Int,Int>()
 arr[0] = 0
 arr[1] = 1
 
-func fibonacci(n:NSDecimalNumber) -> Int{
-    let check = n.stringValue.map{Int(String($0))!}.last!%2==0 ? true : false
+func fibonacci(n:Int) -> Int{
+    let check = n%2==0 ? true : false
     if arr[n] == nil{
         if check{
-            let fnM1 = fibonacci(n: n.dividing(by: 2).subtracting(1))
-            let fn = fibonacci(n: n.dividing(by: 2))
+            let fnM1 = fibonacci(n: n/2-1)
+            let fn = fibonacci(n: n/2)
             arr[n] = (fn*(2*fnM1+fn))%1000000007
         }else{
-            let fnP1 = fibonacci(n: n.adding(1).dividing(by: 2))
-            let fn = fibonacci(n: n.adding(1).dividing(by: 2).subtracting(1))
+            let fnP1 = fibonacci(n: n/2+1)
+            let fn = fibonacci(n: n/2)
             arr[n] = ((fn*fn)+(fnP1*fnP1))%1000000007
         }
     }
     return arr[n]!
 }
-print(fibonacci(n: number))
-
+print(fibonacci(n: n))
--- 2098/2098_swift/2098_swift/main.swift
+++ 2098/2098_swift/2098_swift/main.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 let N = Int(readLine()!)!
-
 var map = Array(repeating: [Int](), count: N)
 var dp = Array(repeating: Array(repeating: -1, count: 1<<N), count: N)
 
@@ -17,25 +16,22 @@ for i in 0..<N{
 }
 
 func TSP(curr:Int, visited:Int) -> Int{
-    let ret = dp[curr][visited]
-    if ret != -1 {
-        return ret
-    }
-
     if visited == (1<<N)-1{
         if map[curr][0] != 0 {
             return map[curr][0]
         }
-        return 10000000000000
+        return 1000000000
+    }
+    
+    if dp[curr][visited] != -1 {
+        return dp[curr][visited]
     }
 
-    dp[curr][visited] = 10000000000000
+    dp[curr][visited] = 1000000000
 
     for i in 0..<N{
-        if visited&(1<<i) == 1 || map[curr][i] == 0{
-            continue
-        }
-        dp[curr][visited] = min(dp[curr][visited], TSP(curr: i, visited: visited|(1<<i))+map[curr][i])
+        if visited&(1<<i) == 1 || map[curr][i] == 0{ continue }
+        dp[curr][visited] = min(dp[curr][visited], TSP(curr: i, visited: visited|(1<<i))+map[curr][i] )
     }
     return dp[curr][visited]
 }
