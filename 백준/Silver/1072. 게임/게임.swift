67570ba0165f52f268862fa57bedc77b6869822a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:29 +0900

    [Silver III] Title: 게임, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let XY = readLine()!.split(separator: " ").map{UInt64(String($0))!}
+var X = XY[0]
+var Y = XY[1]
+let Z = UInt64(100*Y/X)
+
+var cnt = 0
+if Z >= 99{
+    print("-1")
+}else{
+    var left = UInt64(0)
+    var right = UInt64(1000000000)
+    var tempZ = Z
+    while left <= right{
+        let mid = (left+right) / 2
+        tempZ = 100*(Y+mid) / (X+mid)
+        if Z < tempZ{
+            right = mid-1
+        }else{
+            left = mid+1
+        }
+    }
+    print(left)
+}
