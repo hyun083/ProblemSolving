9490182547218f37b1b62aede30a979f2d84bc7c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 00:19:58 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let line = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = line[0]
+let k = line[1]-1
+var arr = Array(repeating: true, count: n+1)
+var ans = [Int]()
+
+
+for i in 2...n{
+    for i in stride(from: i, through: n, by: +i){
+        if arr[i]{
+            arr[i] = false
+            ans.append(i)
+        }
+    }
+}
+print(ans[k])
