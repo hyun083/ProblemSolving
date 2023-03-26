fe79b81e9986fb210683075cd9fcf6e2c746c459
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 00:47:36 +0900

    [Gold V] Title: 동전 2, Time: 32 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let nk = readLine()!.split(separator: " ").map{Int($0)!}
+let n = nk[0]
+var k = nk[1]
+
+var dp = Array(repeating:10001, count:10001)
+dp[0] = 0
+var coins = [Int]()
+for _ in 0..<n{
+    let coin = Int(readLine()!)!
+    if coin <= 10000{ coins.append(coin) }
+}
+
+for coin in coins{
+    for i in coin...10000{
+        dp[i] = min(dp[i], dp[i-coin]+1)
+    }
+}
+if dp[k] == 10001{
+    print(-1)
+}else{
+    print(dp[k])
+}
