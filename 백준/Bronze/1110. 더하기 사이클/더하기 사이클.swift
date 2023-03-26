211e7525c31a1f706d01f693866d470d611d478e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:36 +0900

    [Bronze I] Title: 더하기 사이클, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+var n = readLine()!.map{Int(String($0))!}
+var temp = [Int]()
+var ans = 1
+
+if n.count == 1{
+    n = [0,n[0]]
+    temp = [n[1],n[1]]
+}else{
+    temp = [n[1],(n[0]+n[1])%10]
+}
+ 
+while n != temp{
+    ans += 1
+    let a = temp[0] + temp[1]
+    temp = [temp[1],a%10]
+}
+print(ans)
