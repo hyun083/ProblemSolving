013b417efd5afe3696a698b4fe69243ff9b5d099
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:31 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 12 ms, Memory: 79168 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let x = inputs[0]
+let y = inputs[1]
+let w = inputs[2]
+let h = inputs[3]
+
+let outputs = [x,y,abs(w.distance(to: inputs[0])),abs(h.distance(to: inputs[1]))]
+print(outputs.min()!)
