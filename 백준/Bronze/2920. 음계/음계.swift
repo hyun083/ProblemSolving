dca332a1fd168d4dc90dc2495ce6506c47d2d7c0
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 00:22:23 +0900

    [Bronze II] Title: 음계, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let numbers = readLine()!.split(separator: " ").map{Int($0)!}
+var asc = true
+var des = true
+for i in 0..<numbers.count-1{
+    if numbers[i] < numbers[i+1]{
+        des = false
+    }else if numbers[i] > numbers[i+1]{
+        asc = false
+    }
+}
+if !asc && !des{
+    print("mixed")
+}else if asc{
+    print("ascending")
+}else if des{
+    print("descending")
+}
