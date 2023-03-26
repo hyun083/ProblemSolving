079775fe770005599e1300bcf938ad5dc5be1ffd
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:46 +0900

    [Bronze I] Title: 단어 공부, Time: 168 ms, Memory: 81520 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let text = readLine()!.uppercased()
+var set = [Character:Int]()
+
+for t in text{
+    if set[t] != nil{
+        set[t]! += 1
+    }else{
+        set[t] = 1
+    }
+}
+
+let arr = set.sorted{$0.value>$1.value}
+if arr.count == 1{
+    print(arr[0].key)
+}else if arr[0].value == arr[1].value{
+    print("?")
+}else{
+    print(arr[0].key)
+}
