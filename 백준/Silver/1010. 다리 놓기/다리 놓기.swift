c7a5e2709db17701d4a3fcd6bdd0217cd2864922
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:08 +0900

    [Silver V] Title: 다리 놓기, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.swift
@@ -0,0 +1,13 @@
+for _ in 0..<Int(readLine()!)!{
+    let MN = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let m = MN[0]
+    let n = MN[1]
+    var temp = 1
+    var f = 1
+    for i in stride(from: n, to: n-m, by: -1){
+        temp *= i
+        temp /= f
+        f += 1
+    }
+    print(temp)
+}
