58a3f50ba5acffd5b8ca9cb964a7305d504c2b48
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:33 +0900

    [Silver I] Title: 팔, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let LR = readLine()!.split(separator: " ").map{String($0)}
+let L = LR[0].map{String($0)}
+let R = LR[1].map{String($0)}
+var ans = 0
+
+if L.count != R.count{
+    print(0)
+}else{
+    for i in 0..<L.count{
+        if L[i]==R[i]{
+            if L[i]=="8"{ ans += 1 }
+        }else{
+            break
+        }
+    }
+    print(ans)
+}
