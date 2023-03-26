f335583dc8ae635a1c5157f4b461d281a678f9b9
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 00:22:20 +0900

    [Bronze III] Title: 알람 시계, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+var time = readLine()!.split(separator: " ").map{Int($0)!}
+
+if time[1] >= 45{
+    time[1] -= 45
+}else{
+    time[1] = 60 - (45-time[1])
+    time[0] = (time[0]-1)<0 ? 23:time[0]-1
+}
+print(time[0],time[1])
