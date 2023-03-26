70902aebfdf8fd8684d3bacc3cdeff2e2758d3a4
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:03:13 +0900

    [Silver III] Title: 회전하는 큐, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+var q = Array(1...N)
+var ans = 0
+
+let targets = readLine()!.split(separator: " ").map{Int(String($0))!}
+for target in targets{
+    var left = 0
+    var right = 0
+    for idx in q.indices{
+        if q[idx] == target{
+            left = idx
+            right = q.count-idx
+            break
+        }
+    }
+    if left < right{
+        for _ in 0..<left{
+            q.append(q.removeFirst())
+            ans+=1
+        }
+    }else{
+        for _ in 0..<right{
+            q.insert(q.removeLast(), at: 0)
+            ans+=1
+        }
+    }
+    q.removeFirst()
+}
+print(ans)
