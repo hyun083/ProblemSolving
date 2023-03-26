5739fbf5bdeb42cf71d785e27bdf4e68cd0d2791
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 22:52:28 +0900

    [Gold II] Title: 철로, Time: 60 ms, Memory: 85464 KB -BaekjoonHub

---
+++ 백준/Gold/13334. 철로/철로.swift
@@ -0,0 +1,141 @@
+import Foundation
+
+//by rhyno
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+//by rhyno
+public struct Heap<T> {
+  var nodes: [T] = []
+  let comparer: (T,T) -> Bool
+
+  var isEmpty: Bool {
+      return nodes.isEmpty
+  }
+
+  init(comparer: @escaping (T,T) -> Bool) {
+      self.comparer = comparer
+  }
+
+  func peek() -> T? {
+      return nodes.first
+  }
+
+  mutating func insert(_ element: T) {
+      var index = nodes.count
+
+      nodes.append(element)
+
+      while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
+          nodes.swapAt(index, (index-1)/2)
+          index = (index-1)/2
+      }
+  }
+
+  mutating func delete() -> T? {
+      guard !nodes.isEmpty else {
+          return nil
+      }
+
+      if nodes.count == 1 {
+          return nodes.removeFirst()
+      }
+
+      let result = nodes.first
+      nodes.swapAt(0, nodes.count-1)
+      _ = nodes.popLast()
+
+      var index = 0
+
+      while index < nodes.count {
+          let left = index * 2 + 1
+          let right = left + 1
+
+          if right < nodes.count {
+              if comparer(nodes[left], nodes[right]),
+                  !comparer(nodes[right], nodes[index]) {
+                  nodes.swapAt(right, index)
+                  index = right
+              } else if !comparer(nodes[left], nodes[index]){
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else if left < nodes.count {
+              if !comparer(nodes[left], nodes[index]) {
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else {
+              break
+          }
+      }
+
+      return result
+  }
+}
+
+extension Heap where T: Comparable {
+    init() {
+        self.init(comparer: >)
+    }
+}
+
+let io = FileIO()
+let N = io.readInt()
+var lines = [(h:Int,o:Int)]()
+var startPoint = Heap<Int>()
+var ans = 0
+
+for _ in 0..<N{
+    let h = io.readInt()
+    let o = io.readInt()
+    lines.append(h<o ? (h,o):(o,h))
+}
+
+let D = io.readInt()
+
+lines.sort(by: { return $0.o==$1.o ? ($0.h<$1.h):($0.o<$1.o) })
+
+for line in lines{
+    let length = line.o - line.h
+    if length <= D{
+        startPoint.insert(line.h)
+        while startPoint.peek()! < line.o - D{
+            startPoint.delete()
+        }
+        ans = max(ans, startPoint.nodes.count)
+    }
+}
+print(ans)
