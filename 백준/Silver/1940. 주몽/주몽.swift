248fa21ee3f10f4d8a4b5d15f4e1976e1a4f9966
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:51:56 +0900

    [Silver IV] Title: 주몽, Time: 20 ms, Memory: 80512 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let m = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: {$0 < $1})
+
+var s = 0
+var e = n-1
+var cnt = 0
+
+while s<e{
+    let sum = arr[s] + arr[e]
+    if sum > m{
+        e -= 1
+    }else if sum == m{
+        cnt += 1
+        s += 1
+    }else{
+        s += 1
+    }
+}
+print(cnt)
