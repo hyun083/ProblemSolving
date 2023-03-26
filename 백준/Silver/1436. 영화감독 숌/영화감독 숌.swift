605b1465c64ee363ba9f6fa7ba363d7774eded28
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:31:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 1704 ms, Memory: 79296 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+var num = [Int]()
+
+num.append(666)
+var number = num.last! + 1
+while(num.count <= 10000){
+    let txt = String(number)
+    
+    for index in txt.indices{
+        if txt.distance(from: index, to: txt.index(txt.endIndex, offsetBy: -1)) >= 2 {
+            if txt[index]=="6" && txt[txt.index(index, offsetBy: 1)] == "6" && txt[txt.index(index, offsetBy: 2)] == "6"{
+                num.append(number)
+                break
+            }
+        }else{
+            break
+        }
+    }
+    number += 1
+}
+
+let N = Int(readLine()!)!
+
+print(num[N-1])
