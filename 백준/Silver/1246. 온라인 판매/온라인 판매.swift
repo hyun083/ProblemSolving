8c294379569ac8874f8a4a82c9090f0de2bd8e5f
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-26 23:06:56 +0900

    [Silver IV] Title: 온라인 판매, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+var arr = [Int]()
+var income = 0
+var price = 0
+
+for _ in 0..<M{
+    arr.append(Int(readLine()!)!)
+}
+arr.sort(by: >)
+for i in stride(from: N < arr.count ? N-1 : arr.count-1, through: 0, by: -1){
+    if income < arr[i]*(i+1){
+        income = arr[i]*(i+1)
+        price = arr[i]
+    }
+}
+print(price, income)
