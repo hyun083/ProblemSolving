48a7fc0c151e3956439bf896880e0240abe3867c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 00:47:41 +0900

    [Gold III] Title: 줄 세우기, Time: 872 ms, Memory: 71940 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.swift
@@ -0,0 +1,38 @@
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+var arr = Array(repeating: 0, count: N)
+var map = Array(repeating: [Int](), count:N)
+var queue = [Int]()
+var result = [Int]()
+
+for _ in 0..<M{
+    let numbers = readLine()!.split(separator: " ").map{Int(String($0))! - 1}
+    arr[numbers[1]] += 1
+    map[numbers[0]].append(numbers[1])
+}
+
+for i in 0..<N{
+    if arr[i] == 0{
+        queue.append(i)
+        arr[i] = -1
+    }
+}
+
+while !queue.isEmpty{
+    let idx = queue.removeLast()
+    for number in map[idx]{
+        arr[number] -= 1
+    }
+    result.append(idx)
+    for i in 0..<N{
+        if arr[i] == 0{
+            queue.append(i)
+            arr[i] = -1
+        }
+    }
+}
+
+for result in result {
+    print(result+1, terminator: " ")
+}
