e961e3c0fb4f6ef909a6f8573fe34758f7727f3b
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 01:01:16 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let AB = readLine()!.split(separator: " ").map{Int(String($0))!}
+let A = AB[0]
+let B = AB[1]
+var arr = [Int]()
+
+var cnt = 1
+while arr.count < B{
+    for _ in 1...cnt{
+        arr.append(cnt)
+    }
+    cnt += 1
+}
+
+var result = 0
+for i in A-1...B-1{
+    result += arr[i]
+}
+print(result)
