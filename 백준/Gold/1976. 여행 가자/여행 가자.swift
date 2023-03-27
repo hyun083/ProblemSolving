8e0b2d45ace8b4d7e19f2608736b7e24e29ed458
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:30:07 +0900

    [Gold IV] Title: 여행 가자, Time: 20 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let m = Int(readLine()!)!
+var arr = Array(repeating: -1, count: n)
+func root(of n:Int) -> Int{
+    if arr[n] < 0 {return n}
+    arr[n] = root(of: arr[n])
+    return arr[n]
+}
+func union(a:Int, b:Int){
+    let a = root(of: a)
+    let b = root(of: b)
+    if a != b{
+        arr[b] = a
+    }
+}
+for u in 0..<n{
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    for v in 0..<n{
+        if input[v] == 1{
+            union(a: u, b: v)
+        }
+    }
+}
+let cities = readLine()!.split(separator: " ").map{root(of: Int(String($0))! - 1)}
+var flag = true
+for city in cities{
+    if city == cities[0]{
+        continue
+    }else{
+        flag = false
+        break
+    }
+}
+if flag{
+    print("YES")
+}else{
+    print("NO")
+}
