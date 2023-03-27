1193d8c6369a428000e8bf79ac1f0aaae7f45c1a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 17:39:26 +0900

    [Silver I] Title: RGB거리, Time: 12 ms, Memory: 79168 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let N = Int(readLine()!)!
+var ans = Array(repeating: Array(repeating: 0, count: 3), count: N+1)
+
+for i in 1...N{
+    let cost =  readLine()!.split(separator: " ").map{Int(String($0))!}
+    ans[i][0] = min(ans[i-1][1], ans[i-1][2]) + cost[0]
+    ans[i][1] = min(ans[i-1][0], ans[i-1][2]) + cost[1]
+    ans[i][2] = min(ans[i-1][0], ans[i-1][1]) + cost[2]
+}
+print(min(ans[N][2], min(ans[N][0], ans[N][1])))
