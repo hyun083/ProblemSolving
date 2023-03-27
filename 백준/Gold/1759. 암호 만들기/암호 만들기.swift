c5f0d3eb1dd6ae7a0e405bd5315eb6a6969bf8db
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:36:29 +0900

    [Gold V] Title: 암호 만들기, Time: 16 ms, Memory: 79172 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let L = inputs[0]
+let C = inputs[1]
+var arr = readLine()!.split(separator: " ").map{String($0)}
+arr.sort(by: <)
+var pwd = [String](repeating: "", count: 16)
+
+
+func backTracking(pos:Int, prev:Int, consonant:Int, vowel:Int){
+    if pos == L{
+        if consonant >= 2 && vowel >= 1 {
+            for p in pwd{
+                print(p,terminator: "")
+            }
+            print()
+        }
+        return
+    }
+    
+    for i in prev+1..<C{
+        pwd[pos] = arr[i]
+        if arr[i] == "a" || arr[i] == "e" || arr[i] == "i" || arr[i] == "o" || arr[i] == "u"{
+            backTracking(pos: pos+1, prev: i, consonant: consonant, vowel: vowel + 1)
+        }else{
+            backTracking(pos: pos+1, prev: i, consonant: consonant + 1, vowel: vowel)
+        }
+    }
+}
+
+backTracking(pos: 0, prev: -1, consonant: 0, vowel: 0)
