f6c96a493da374436a3214e0de806c20c15c765e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:19:42 +0900

    [Gold V] Title: 트리, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+let N = Int(readLine()!)!
+var arr = Array(repeating: [Int](), count: N)
+var start = 0
+var flag = false
+
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+for i in 0..<N{
+    if input[i] != -1{
+        arr[input[i]].append(i)
+    }else{
+        start = i
+    }
+}
+
+let node = Int(readLine()!)!
+
+func delete(node:Int){
+    if node == start{
+        flag = true
+        return
+    }
+    let idx = input[node]
+    arr[idx].remove(at: arr[idx].firstIndex(of: node)!)
+}
+
+
+var ans = 0
+func dfs(from node:Int){
+    if arr[node].count == 0{
+        ans += 1
+        return
+    }else{
+        for next in arr[node]{
+            dfs(from: next)
+        }
+    }
+}
+delete(node: node)
+dfs(from: start)
+if flag == true{
+    print(0)
+}else{
+    print(ans)
+}
