8c48d44dbda5da688bc4d91aeeef6b24b6c43a05
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:37:34 +0900

    [Gold IV] Title: 트리의 지름, Time: 36 ms, Memory: 82364 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+let N = Int(readLine()!)!
+var tree = Array(repeating: [Int:Int](), count: N)
+var visited = Array(repeating: false, count: N)
+
+for _ in 1..<N{
+    let edge = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let node0 = edge[0] - 1
+    let node1 = edge[1] - 1
+    let weight = edge[2]
+    tree[node0][node1] = weight
+    tree[node1][node0] = weight
+}
+
+var diameter = 0
+var farderNode = 0
+
+func dfs(node:Int, weight:Int){
+    if visited[node]{
+        return
+    }
+    visited[node] = true
+    if diameter < weight{
+        diameter = weight
+        farderNode = node
+    }
+    for n in tree[node]{
+        dfs(node: n.key, weight: weight+n.value)
+    }
+}
+dfs(node: 0, weight: 0)
+visited = Array(repeating: false, count: N)
+diameter = 0
+dfs(node: farderNode, weight: diameter)
+print(diameter)
