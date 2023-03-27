4453c19dc42028963f4a440f566943d3febd22bf
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 17:39:34 +0900

    [Silver V] Title: 단어 나누기, Time: 24 ms, Memory: 79644 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let str = readLine()!.map{String($0)}
+var start = 0
+var mid = 1
+var end = 2
+var ans = [String]()
+
+while end <= str.count-1{
+    for i in mid+1..<str.count{
+        var temp = str[start..<mid].reversed().joined()
+        temp += str[mid..<i].reversed().joined()
+        temp += str[i..<str.count].reversed().joined()
+        ans.append(temp)
+    }
+    mid += 1
+    end += 1
+}
+print(ans.sorted(by: <)[0])
