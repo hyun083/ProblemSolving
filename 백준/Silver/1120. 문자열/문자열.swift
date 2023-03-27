a31b850adbb1e69b487ce34c5b3b390ac60b9274
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 17:44:05 +0900

    [Silver IV] Title: 문자열, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+let AB = readLine()!.split(separator: " ").map{String($0)}
+let A = AB[0].map{String($0)}
+let B = AB[1].map{String($0)}
+var cnt = 51
+
+for i in 0...B.count - A.count{
+    var temp = 0
+    for k in 0..<A.count{
+        if A[k] != B[k+i]{
+            temp += 1
+        }
+    }
+    cnt = temp<cnt ? temp:cnt
+}
+print(cnt)
