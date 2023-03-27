b9b8260fec860a5b46d566fc9deb672463cba02b
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:27:48 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let input = readLine()!
+var number = ""
+var numbers = [Int]()
+var minusFlag = false
+
+for text in input{
+    
+    if text == "+" || text == "-"{
+        if minusFlag{
+            numbers.append(Int(number)! * -1)
+        }else{
+            numbers.append(Int(number)!)
+        }
+        if text == "-"{
+            minusFlag = true
+        }
+    }else{
+        number.append(text)
+        continue
+    }
+    number = ""
+}
+if minusFlag{
+    numbers.append(Int(number)! * -1)
+}else{
+    numbers.append(Int(number)!)
+}
+print(numbers.reduce(0, +))
