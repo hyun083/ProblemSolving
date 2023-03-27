6bcefa21f0603f2cd83ffbf7d8af8610e6284b15
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:11:06 +0900

    [Silver I] Title: 물병, Time: 348 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/1052. 물병/물병.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+let nk = readLine()!.split(separator: " ").map{Int(String($0))!}
+var n = nk[0]
+let k = nk[1]
+if n<=k{
+    print(0)
+}else{
+    var ans = 0
+    while true{
+        var cnt = 0
+        var temp = n
+        while temp > 0{
+            if temp%2==0{
+                temp /= 2
+            }else{
+                temp /= 2
+                cnt += 1
+            }
+        }
+        if cnt <= k { break }
+        ans += 1
+        n += 1
+    }
+    print(ans)
+}
