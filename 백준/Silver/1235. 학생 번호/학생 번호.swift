0ea2517dc2df02761d7dc51ca5657533b10d574e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-28 00:12:16 +0900

    [Silver IV] Title: 학생 번호, Time: 24 ms, Memory: 80836 KB -BaekjoonHub

---
+++ 백준/Silver/1235. 학생 번호/학생 번호.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+var numbers = [[String]]()
+var N = Int(readLine()!)!
+for _ in 0..<N{
+    numbers.append(readLine()!.map{String($0)})
+}
+let n = numbers[0].count
+var ans = n-1
+var flag = false
+
+while !flag{
+    var check = [String:Bool]()
+    var flag2 = true
+    
+    for i in 0..<N{
+        if check[numbers[i][ans..<n].joined()] == nil{
+            check[numbers[i][ans..<n].joined()] = true
+        }else{
+            flag2 = false
+            break
+        }
+    }
+    if !flag2{
+        ans -= 1
+    }
+    flag = flag2
+}
+print(n-ans)
