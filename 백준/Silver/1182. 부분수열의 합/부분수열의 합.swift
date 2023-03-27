86affb8de4511bc1f91dfb7e9e068506a0ca72d5
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-27 17:44:09 +0900

    [Silver II] Title: 부분수열의 합, Time: 28 ms, Memory: 79168 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let N = inputs[0]
+let S = inputs[1]
+
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var cnt = 0
+var total = 0
+
+func dfs(from curr:Int){
+    if curr == N {return}
+    if total + arr[curr] == S {cnt += 1}
+    
+    dfs(from: curr+1)
+    
+    total += arr[curr]
+    dfs(from: curr+1)
+    
+    total -= arr[curr]
+}
+
+dfs(from: 0)
+print(cnt)
