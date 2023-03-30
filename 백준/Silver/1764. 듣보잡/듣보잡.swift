9d2bd1fb380cf383aa89a075afe628b1f3b86d00
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-31 01:33:13 +0900

    [Silver IV] Title: 듣보잡, Time: 88 ms, Memory: 86300 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+
+var people:[String:Bool] = [:]
+var ans = [String]()
+
+for _ in 0..<N{
+    people[readLine()!] = true
+}
+
+for _ in 0..<M{
+    let person = readLine()!
+    if people[person] != nil {
+        ans.append(person)
+    }
+}
+
+print(ans.count)
+print(ans.sorted().joined(separator: "\n"))
