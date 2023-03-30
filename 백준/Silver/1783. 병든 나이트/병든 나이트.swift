9f5265463fe81b66d1a389028e550093f4186da4
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-31 01:33:35 +0900

    [Silver III] Title: 병든 나이트, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0]
+let m = nm[1]
+
+if n==1{
+    print(1)
+}else if n==2{
+    print(min(4,(m+1)/2))
+}else{
+    if m<7{
+        print(min(4, m))
+    }else{
+        print(m-2)
+    }
+}
