49bea24e1f72900d25e66cabe94b8bfeb9fefe46
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-03-31 01:35:26 +0900

    [Silver III] Title: 놀라운 문자열, Time: 60 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/1972. 놀라운 문자열/놀라운 문자열.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+while let input = readLine(){
+    if input == "*"{ break }
+    let input = input.map{String($0)}
+    let n = input.count
+    var flag = true
+    if n>1{
+        for D in 1...n-1{
+            var s = Set<String>()
+            for i in 0..<n-D{
+                let pair = input[i]+input[i+D]
+                if !s.contains(pair){
+                    s.insert(pair)
+                }else{
+                    flag = false
+                }
+            }
+        }
+    }
+    if flag{
+        print(input.joined(),"is surprising.")
+    }else{
+        print(input.joined(),"is NOT surprising.")
+    }
+}
