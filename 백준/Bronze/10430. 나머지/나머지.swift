aa4a52444288e7f49fbb7ae34f05a39be9a87290
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:33:45 +0900

    [Bronze V] Title: 나머지, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -0,0 +1,9 @@
+let numbers = readLine()!.split(separator: " ").map{Int(String($0))!}
+let A = numbers[0]
+let B = numbers[1]
+let C = numbers[2]
+
+print((A+B)%C)
+print((A+B)%C)
+print((A*B)%C)
+print((A*B)%C)
