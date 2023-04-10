1b6f4a76d51f90ce7e1795e2292d35cb73b36548
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:54:02 +0900

    [Gold III] Title: 전국시대, Time: 152 ms, Memory: 83192 KB -BaekjoonHub

---
+++ 백준/Gold/15809. 전국시대/전국시대.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map{Int($0)!}
+let n = nm[0]
+let m = nm[1]
+
+var arr = Array(repeating: (-1,0), count: n)
+func root(of a:Int)-> Int{
+    if arr[a].0 < 0 {return a}
+    arr[a].0 = root(of: arr[a].0)
+    return arr[a].0
+}
+
+func union(a:Int, b:Int){
+    let rootA = root(of: a)
+    let rootB = root(of: b)
+    if rootA == rootB {return}
+    if arr[rootA].1 >= arr[rootB].1{
+        arr[rootA].1 += arr[rootB].1
+        arr[rootB].0 = rootA
+        arr[rootB].1 = 0
+    }else{
+        arr[rootB].1 += arr[rootA].1
+        arr[rootA].0 = rootB
+        arr[rootA].1 = 0
+    }
+}
+
+func war(a:Int, b:Int){
+    let rootA = root(of: a)
+    let rootB = root(of: b)
+    if arr[rootA].1 > arr[rootB].1{
+        arr[rootA].1 -= arr[rootB].1
+        arr[rootB].0 = rootA
+        arr[rootB].1 = 0
+    }else if arr[rootA] == arr[rootB]{
+        arr[rootA].1 = 0
+        arr[rootB].1 = 0
+    }else{
+        arr[rootB].1 -= arr[rootA].1
+        arr[rootA].0 = rootB
+        arr[rootA].1 = 0
+    }
+}
+
+for i in 0..<n{ arr[i].1 = Int(readLine()!)! }
+for _ in 0..<m{
+    let info = readLine()!.split(separator: " ").map{Int($0)!}
+    if info[0] == 1{
+        union(a: info[1]-1, b: info[2]-1)
+    }else{
+        war(a: info[1]-1, b: info[2]-1)
+    }
+}
+var ans = [Int]()
+for i in 0..<n{
+    if arr[i].1 > 0{
+        ans.append(arr[i].1)
+    }
+}
+print(ans.count)
+for ans in ans.sorted(by: <){ print(ans,terminator: " ") }
