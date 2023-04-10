e0a44a785ff33cc6b4ec766b174674670ab92358
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:33:25 +0900

    [Silver IV] Title: Maximum Subarray, Time: 24 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+for _ in 0..<Int(readLine()!)!{
+    let n = Int(readLine()!)!
+    var temp = 0
+    var ans = Int.min
+    let psum = [0] + readLine()!.split(separator: " ").map{num in
+        temp += Int(num)!
+        ans = max(ans, Int(num)!)
+        return temp
+    }
+    
+    for i in 1..<n{
+        for k in i+1...n{
+            ans = max(ans, psum[k]-psum[i-1])
+        }
+    }
+    print(ans)
+}
