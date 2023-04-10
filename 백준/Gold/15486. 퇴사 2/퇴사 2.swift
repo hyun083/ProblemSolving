3a2e55972fa5aae26a087184c3e9a3f79e2f9268
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:51:43 +0900

    [Gold V] Title: 퇴사 2, Time: 1800 ms, Memory: 91236 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n+1)
+var myMax = 0
+for i in 0..<n{
+    let tp = readLine()!.split(separator: " ").map{Int($0)!}
+    let t = tp[0]
+    let p = tp[1]
+    myMax = max(myMax, dp[i])
+    if i+t <= n{
+        dp[i+t] = max(dp[i+t], myMax+p)
+    }
+}
+print(dp.max()!)
