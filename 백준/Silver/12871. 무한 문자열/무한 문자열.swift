b5c02d3be7cf2cd62357a2b653df6048b40faf66
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:45:21 +0900

    [Silver V] Title: 무한 문자열, Time: 12 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let F = readLine()!
+let S = readLine()!
+var f = ""
+var s = ""
+
+func gcd(num1:Int, num2:Int) -> Int{
+    if num2 == 0{
+        return num1
+    }else{
+        return gcd(num1: num2, num2: num1 % num2)
+    }
+}
+
+func LCM(num1:Int, num2:Int) -> Int{
+    return num1 * num2 / gcd(num1: num1, num2: num2)
+}
+
+let ans = LCM(num1: F.count, num2: S.count)
+
+for _ in 0..<ans/F.count{
+    f += F
+}
+
+for _ in 0..<ans/S.count{
+    s += S
+}
+
+let result = f==s ? "1":"0"
+print(result)
