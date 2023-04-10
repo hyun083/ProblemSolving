231f5a86f932b8861be41a4e5fe5740d97f3039f
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:49:14 +0900

    [Silver III] Title: 퇴사, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct Work{
+    let t:Int
+    let p:Int
+}
+let n = Int(readLine()!)!
+var works = [Work]()
+var ans = [Int]()
+for _ in 0..<n{
+    let tp = readLine()!.split(separator: " ").map{Int(String($0))!}
+    works.append(Work(t: tp[0], p: tp[1]))
+}
+func result(from day:Int)->Int{
+    var total = 0
+    if day + works[day].t <= n{
+        total += works[day].p
+    }
+    if day + works[day].t < n{
+        var temp = 0
+        for i in day + works[day].t..<n{
+            temp = max(temp, result(from: i))
+        }
+        total += temp
+    }
+    return total
+}
+for i in 0..<n{
+    ans.append(result(from: i))
+}
+print(ans.max()!)
