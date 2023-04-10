771fb354e7bfbff87806f78f3af9aa6badadec0c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:11:36 +0900

    [Gold V] Title: 두 용액, Time: 72 ms, Memory: 88760 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
+var start = 0
+var end = N-1
+var result = Int.max
+var idx1 = 0
+var idx2 = 0
+
+while start < end{
+    let number = arr[start]+arr[end]
+    if result > abs(number){
+        result = abs(number)
+        idx1 = start
+        idx2 = end
+    }
+    if number<0{
+        start += 1
+    }else if number>0{
+        end -= 1
+    }else{
+        idx1 = start
+        idx2 = end
+        break
+    }
+}
+print(arr[idx1], arr[idx2])
