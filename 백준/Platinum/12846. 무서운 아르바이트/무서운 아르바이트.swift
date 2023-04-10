5e92a21510f717a40d8a681f119730ca8c304b7e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:44:54 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 84 ms, Memory: 87376 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int($0)!}
+var tree = Array(repeating: -1, count: N*4)
+var ans = 0
+
+func init_tree(start:Int, end:Int, node:Int) -> Int{
+    if start == end{
+        tree[node] = start
+        return tree[node]
+    }
+    let mid = (start+end)/2
+    let left = init_tree(start: start, end: mid, node: node*2)
+    let right = init_tree(start: mid+1, end: end, node: node*2+1)
+    
+    if arr[left] == arr[right]{
+        tree[node] = left<right ? left:right
+    }else{
+        tree[node] = arr[left]<arr[right] ? left:right
+    }
+    return tree[node]
+}
+
+func read_tree(start:Int, end:Int, node:Int, from:Int, to:Int) -> Int{
+    if end < from || to < start{
+        return -1
+    }
+    if from <= start && end <= to{
+        return tree[node]
+    }
+    let mid = (start+end)/2
+    let left = read_tree(start: start, end: mid, node: node*2, from: from, to: to)
+    let right = read_tree(start: mid+1, end: end, node: node*2+1, from: from, to: to)
+    
+    if left < 0 {
+        return right
+    }else if right < 0{
+        return left
+    }else if arr[left] == arr[right]{
+        return left<right ? left:right
+    }else{
+        return arr[left]<arr[right] ? left:right
+    }
+}
+
+func update_ans(from:Int, to:Int){
+    let idx = read_tree(start: 0, end: N-1, node: 1, from: from, to: to)
+    let date = to-from+1
+    let salary = arr[idx]
+    ans = max(date*salary, ans)
+    
+    if from < idx{
+        update_ans(from: from, to: idx-1)
+    }
+    if idx < to{
+        update_ans(from: idx+1, to: to)
+    }
+}
+
+init_tree(start: 0, end: N-1, node: 1)
+update_ans(from: 0, to: N-1)
+print(ans)
