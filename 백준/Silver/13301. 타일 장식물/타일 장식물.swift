622fa2f17bc2490d0daa1bebb6175c7bb5c1d10b
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:45:55 +0900

    [Silver V] Title: 타일 장식물, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+var n = Int(readLine()!)!
+var arr = Array(repeating: 0, count: 81)
+
+func fibonacci(num:Int) -> Int{
+    if num == 0 || num == 1{
+        return 1
+    }
+    if arr[num] != 0 {
+        return arr[num]
+    }else{
+        let temp = fibonacci(num: num-1) + fibonacci(num: num-2)
+        arr[num] = temp
+        return temp
+    }
+}
+
+if n==1{
+    print("4")
+}else{
+    let result = fibonacci(num: n-1)*2 + (fibonacci(num: n-2)+fibonacci(num: n-1))*2
+    print(result)
+}
