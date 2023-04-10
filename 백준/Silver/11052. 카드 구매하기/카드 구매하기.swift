7fd655823455dd9af51a34083b9cc2c90234687c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:39:24 +0900

    [Silver I] Title: 카드 구매하기, Time: 24 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+var arr = [0]
+let N = Int(readLine()!)!
+var dp = Array(repeating: 0, count: N+1)
+
+arr += readLine()!.split(separator: " ").map{Int(String($0))!}
+
+for i in 1...N{
+    for k in 1...i{
+        dp[i] = max(dp[i], dp[i-k] + arr[k])
+    }
+}
+print(dp[N])
