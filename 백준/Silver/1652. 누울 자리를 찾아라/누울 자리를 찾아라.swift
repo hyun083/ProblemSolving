c1239868222217dda93bd1cc79ef2367dc6055e3
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 01:56:58 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 20 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var horizen = Array(repeating: "", count: n)
+var vertical = Array(repeating: "", count: n)
+var ans = [0,0]
+
+for i in 0..<n{
+    let line = readLine()!.map{String($0)}
+    for k in 0..<n{
+        horizen[i] += line[k]
+        vertical[k] += line[k]
+    }
+}
+
+for i in 0..<n{
+    let h = horizen[i].split(separator: "X")
+    let v = vertical[i].split(separator: "X")
+    
+    for space in h{
+        if space.contains(".."){ ans[0] += 1}
+    }
+    for space in v{
+        if space.contains(".."){ ans[1]+=1 }
+    }
+}
+print(ans[0],ans[1])
