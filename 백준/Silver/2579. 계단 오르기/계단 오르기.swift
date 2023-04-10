4ae1a4c606e67f3e462f7a60fbcaa128e4b64323
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:13:39 +0900

    [Silver III] Title: 계단 오르기, Time: 12 ms, Memory: 79164 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let N = Int(readLine()!)!
+var arr = Array(repeating: 0, count: N+1)
+var dp = Array(repeating: 0, count: N+1)
+
+for i in 1...N{
+    arr[i] = Int(readLine()!)!
+}
+
+if N == 1{
+    print(arr[1])
+}else if N == 2{
+    print(arr[1] + arr[2])
+}else{
+    dp[1] = arr[1]
+    dp[2] = arr[1]+arr[2]
+    for i in 3...N{
+        dp[i] = max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i])
+    }
+    print(dp[N])
+}
