720d103f0a8c7e7f4ee5391292355d9ff207f7ba
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:43:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 636 ms, Memory: 88208 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0]
+let m = nm[1]
+var arr = Array(repeating: [Int](), count: n)
+var visited = Array(repeating: false, count: n)
+
+for _ in 0..<m{
+    let uv = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let u = uv[0]-1
+    let v = uv[1]-1
+    arr[u].append(v)
+    arr[v].append(u)
+}
+
+func dfs(from n: Int){
+    let curr = n
+    visited[curr] = true
+    for next in arr[curr]{
+        if !visited[next]{
+            dfs(from: next)
+        }
+    }
+}
+var components = 0
+for i in 0..<n{
+    if !visited[i]{
+        components += 1
+        dfs(from: i)
+    }
+}
+print(components)
