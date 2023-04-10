45235c5498cd4387b7c7e4bdf9ae7e6e729e4beb
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:39:43 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var dp = arr
+for i in 0..<n{
+    for k in 0..<i{
+        if arr[i]>arr[k] && dp[i]<dp[k]+arr[i]{
+            dp[i] = dp[k]+arr[i]
+        }
+    }
+}
+print(dp.max()!)
