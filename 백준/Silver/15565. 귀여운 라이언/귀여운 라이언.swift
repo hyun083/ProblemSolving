60c8e941e99be637e7077405a4990c85060cb580
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:51:49 +0900

    [Silver I] Title: 귀여운 라이언, Time: 192 ms, Memory: 132760 KB -BaekjoonHub

---
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let nk = readLine()!.split(separator: " ").map{Int($0)!}
+let n = nk[0]
+let k = nk[1]
+
+var map = readLine()!.split(separator: " ").map{Int($0)!}
+
+var head = 0
+var tail = 0
+var cnt = 0
+var ans = n+1
+
+while true{
+    if cnt >= k{
+        if map[head] == 1{ cnt -= 1 }
+        head += 1
+    }else if tail == n{
+        break
+    }else{
+        if map[tail] == 1{ cnt += 1 }
+        tail += 1
+    }
+    if cnt==k{
+        let length = tail - head
+        ans = min(ans, length)
+    }
+}
+print(ans==n+1 ? -1:ans)
