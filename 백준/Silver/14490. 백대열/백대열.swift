225851d6657e4d70a22e18b9af7da9eb89b504d4
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:48:47 +0900

    [Silver V] Title: 백대열, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/14490. 백대열/백대열.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func gcd(_ num1:Int,_ num2:Int) -> Int{
+    if num1%num2 == 0{
+        return num2
+    }else{
+        return gcd(num2,num1%num2)
+    }
+}
+
+let nm = readLine()!.split(separator: ":").map{Int(String($0))!}
+let n = nm[0]
+let m = nm[1]
+
+if n==m{
+    print("1:1")
+}else{
+    let temp = n>m ? gcd(n, m):gcd(m, n)
+    print("\(n/temp):\(m/temp)")
+}
