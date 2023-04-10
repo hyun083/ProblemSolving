055e69a2bd86907e954a100422d25ed426ddbe6a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:53:16 +0900

    [Silver II] Title: N과 M (12), Time: 16 ms, Memory: 79176 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let N = inputs[0]
+let M = inputs[1]
+
+var num = Array(Set(readLine()!.split(separator: " ").map{Int(String($0))!})).sorted(by: <)
+
+var arr = [String]()
+
+func btk(numb:Int, cnt:Int){
+    if cnt == M{
+        if arr.count == M{
+            print(arr.joined(separator: " "))
+        }
+        return
+    }
+    
+    for i in numb..<num.count{
+        arr.append(String(num[i]))
+        btk(numb: i, cnt: cnt+1)
+        arr.removeLast()
+    }
+}
+
+btk(numb: 0, cnt: 0)
