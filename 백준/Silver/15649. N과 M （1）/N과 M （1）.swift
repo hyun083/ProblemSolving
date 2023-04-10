e5d19a2ad7157b98fb6c14803d268db5b36da0fc
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:51:56 +0900

    [Silver III] Title: N과 M (1), Time: 132 ms, Memory: 79168 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = inputs[0]
+let M = inputs[1]
+
+var numbers = Array(1...N).map{String($0)}
+var arr = Array(repeating: "", count: M)
+var visited = Array(repeating: false, count: N)
+
+func dfs(cnt:Int){
+    if cnt == M{
+        for a in arr{
+            print(a,terminator: " ")
+        }
+        print()
+        return
+    }
+    
+    for i in 0..<N{
+        if visited[i] == false{
+            visited[i] = true
+            arr[cnt] = numbers[i]
+            dfs(cnt: cnt + 1)
+            visited[i] = false
+            
+        }
+    }
+}
+
+dfs(cnt: 0)
