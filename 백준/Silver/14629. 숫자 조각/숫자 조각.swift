4a8f8c3f9f3010d446a8f4f377291bfbf1f962b3
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:50:03 +0900

    [Silver I] Title: 숫자 조각, Time: 752 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/14629. 숫자 조각/숫자 조각.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+let n = readLine()!.map{String($0)}
+let N = Int(n.joined())!
+var ans = Int.min
+var diff = Int.max
+
+var visited = Array(repeating: false, count: 10)
+var temp = 0
+
+func btk(cnt:Int){
+    if cnt == n.count{
+        if abs(temp-N) < diff{
+            diff = abs(temp-N)
+            ans = temp
+        }
+        return
+    }
+    for i in 0..<10{
+        if !visited[i]{
+            visited[i] = true
+            temp *= 10
+            temp += i
+            btk(cnt: cnt+1)
+            temp -= i
+            temp /= 10
+            visited[i] = false
+        }
+    }
+}
+if N >= 9876543210{
+    print(9876543210)
+}else{
+    btk(cnt: 0)
+    print(ans)
+}
