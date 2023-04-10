352b00a676ca4579c1821545363d22c44603e4e2
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 03:00:09 +0900

    [Silver II] Title: 좌표 압축, Time: 1064 ms, Memory: 154992 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let N = Int(readLine()!)!
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+let numbers = Set<Int>(inputs).sorted(by: <)
+var dict:[Int:Int] = [:]
+
+for idx in 0..<numbers.count{
+    dict[numbers[idx]] = idx
+}
+var ans = ""
+for i in inputs{
+    ans += String(dict[i]!) + " "
+}
+print(ans)
