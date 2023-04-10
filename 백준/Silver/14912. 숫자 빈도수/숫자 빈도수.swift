a61e9b62a639016c3cff6253e7350a7e3ecbc8cd
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:51:10 +0900

    [Silver V] Title: 숫자 빈도수, Time: 64 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let line = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = line[0]
+let target = String(line[1])
+var ans = 0
+
+for number in 1...n{
+    let number = String(number).map{String($0)}
+    for n in number{
+        if n == target{
+            ans += 1
+        }
+    }
+}
+print(ans)
