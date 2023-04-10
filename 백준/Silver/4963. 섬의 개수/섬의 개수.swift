b0fd7c5c29b13552e8e769222855969010b6657f
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:23:28 +0900

    [Silver II] Title: 섬의 개수, Time: 20 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+while let line = readLine(){
+    if line == "0 0"{
+        break
+    }
+    let wh = line.split(separator: " ").map{Int($0)!}
+    let w = wh[0]
+    let h = wh[1]
+    var map = [[Int]]()
+    var visited = Array(repeating: Array(repeating: false, count: w), count: h)
+    var ans = 0
+    
+    for _ in 0..<h{
+        map.append(readLine()!.split(separator: " ").map{Int($0)!})
+    }
+    
+    let dx = [-1,-1,-1,0,0,1,1,1]
+    let dy = [-1,0,1,-1,1,-1,0,1]
+    var q = [[Int]]()
+    var dq = [[Int]]()
+    func bfs(){
+        while !q.isEmpty{
+            dq = q.reversed()
+            q.removeAll()
+            for _ in 0..<dq.count{
+                let curr = dq.removeLast()
+                let x = curr[0]
+                let y = curr[1]
+                for i in 0..<8{
+                    let nx = x+dx[i]
+                    let ny = y+dy[i]
+                    if 0<=nx && nx<h && 0<=ny && ny<w{
+                        if !visited[nx][ny] && map[nx][ny]==1{
+                            visited[nx][ny] = true
+                            q.append([nx,ny])
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    for x in 0..<h{
+        for y in 0..<w{
+            if !visited[x][y] && map[x][y]==1{
+                visited[x][y] = true
+                q.append([x,y])
+                bfs()
+                ans += 1
+            }
+        }
+    }
+    print(ans)
+}
