de89856db62bfbf96e9039e4d660970da06314e7
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 03:05:51 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 16 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var dancePeople = Set<String>()
+dancePeople.insert("ChongChong")
+
+for _ in 0..<n{
+    let people = readLine()!.split(separator: " ").map{String($0)}
+    if dancePeople.contains(people[0]) || dancePeople.contains(people[1]){
+        dancePeople.insert(people[0])
+        dancePeople.insert(people[1])
+    }
+}
+print(dancePeople.count)
