165797f3191b2f6b114f1011591753e4c7d0f9ac
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:12:10 +0900

    [Silver IV] Title: 수열, Time: 48 ms, Memory: 86052 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var ans = 0
+var start = 0
+var cnt = 1
+while start < n-1{
+    if arr[start] <= arr[start+1]{
+        cnt += 1
+    }else{
+        ans = max(ans, cnt)
+        cnt = 1
+    }
+    start += 1
+}
+ans = max(ans, cnt)
+
+start = 0
+cnt = 1
+while start < n-1{
+    if arr[start] >= arr[start+1]{
+        cnt += 1
+    }else{
+        ans = max(ans, cnt)
+        cnt = 1
+    }
+    start += 1
+}
+ans = max(ans, cnt)
+print(ans)
