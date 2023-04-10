c990965b94fff4635ceb203f7787b90a75c31e9f
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:12:25 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let N = Int(readLine()!)!
+
+var star = ""
+for i in 1...(2*N-1){
+    if i < N{
+        for _ in 0..<N-i{
+            star += " "
+        }
+        for _ in 0..<i{
+            star += "*"
+        }
+    }else if i == N{
+        for _ in 0..<N{
+            star += "*"
+        }
+    }else{
+        let temp = i-N
+        for _ in 0..<temp{
+            star += " "
+        }
+        for _ in 0..<N-temp{
+            star += "*"
+        }
+    }
+    if i != 2*N-1{
+        star += "\n"
+    }
+}
+print(star)
