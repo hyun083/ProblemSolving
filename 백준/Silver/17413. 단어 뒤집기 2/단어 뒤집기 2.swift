011066c37da4abc0e2229be6911cf34325f3f694
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:58:18 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 40 ms, Memory: 79976 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+let txt = readLine()!
+var result = ""
+var temp = ""
+var flag = false
+
+for t in txt{
+    if t == "<"{
+        result += String(temp.reversed()) + "<"
+        temp = ""
+        flag = true
+        continue
+    }
+    
+    if t == ">"{
+        flag = false
+        result.append(">")
+        continue
+    }
+    
+    if flag{
+        result.append(t)
+        continue
+    }else{
+        if t == " "{
+            result += String(temp.reversed()) + " "
+            temp = ""
+            continue
+        }
+        temp.append(t)
+        continue
+    }
+}
+if temp != ""{
+    result += String(temp.reversed())
+}
+print(result)
