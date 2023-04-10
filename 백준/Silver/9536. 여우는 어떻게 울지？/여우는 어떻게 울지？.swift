200f33f9842ff8adb88ce22056266085e643fea7
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:31:06 +0900

    [Silver IV] Title: 여우는 어떻게 울지?, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let T = Int(readLine()!)!
+
+for _ in 0..<T{
+    var sound = readLine()!.split(separator: " ").map{String($0)}
+    while let line = readLine(){
+        if line == "what does the fox say?"{
+            break
+        }
+        let otherSound = line.split(separator: " ").map{String($0)}[2]
+        for i in 0..<sound.count{
+            if sound[i] == otherSound{
+                sound[i] = ""
+            }
+        }
+    }
+    let res = sound.filter({$0 != ""})
+    print(res.joined(separator: " "))
+}
