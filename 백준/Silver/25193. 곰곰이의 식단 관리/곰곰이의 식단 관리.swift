52beef4f08aa19aed524dcb60afa8da8d0767f99
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 03:04:18 +0900

    [Silver V] Title: 곰곰이의 식단 관리, Time: 16 ms, Memory: 79708 KB -BaekjoonHub

---
+++ 백준/Silver/25193. 곰곰이의 식단 관리/곰곰이의 식단 관리.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let foods = readLine()!
+var c = 0
+var temp = 1
+
+for food in foods{
+    if food == "C"{
+        c += 1
+    }else{
+        temp += 1
+    }
+}
+
+if c == n{
+    print(n)
+}else{
+    if c%temp == 0{
+        print(c/temp)
+    }else{
+        print(c/temp+1)
+    }
+}
