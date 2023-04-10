0879d0069ac142dbf90e4aab242a25b78863238c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:18:15 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+var arr = [Int]()
+var sum = 0
+var ans = Int.max
+
+for _ in 0..<10{
+    let score = Int(readLine()!)!
+    sum += score
+    arr.append(sum)
+}
+
+for i in 0..<10{
+    if abs(ans-100) > abs(arr[i]-100){
+        ans = arr[i]
+    }else if abs(ans-100) == abs(arr[i]-100){
+        ans = max(ans, arr[i])
+    }else{
+        continue
+    }
+}
+print(ans)
