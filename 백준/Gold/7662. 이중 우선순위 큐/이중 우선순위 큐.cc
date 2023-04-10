978cbadb4b548aff6c14ba988f7ba66837817687
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:28:12 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2664 ms, Memory: 48884 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int T,K;
+char cmd;
+
+int main() {
+    cin >> T;
+    for(int i = 0; i < T; i++){
+        multiset<int> arr;
+        cin >> K;
+        for(int k = 0; k<K; k++){
+            int data;
+            cin >> cmd >> data;
+            if (cmd == 'I'){
+                arr.insert(data);
+            }else{
+                if(arr.empty()){
+                    continue;
+                }else{
+                    if (data == 1){
+                        arr.erase(--arr.end());
+                    }else{
+                        arr.erase(arr.begin());
+                    }
+                }
+            }
+        }
+        if (arr.empty()){
+            cout << "EMPTY" << "\n";
+        }else{
+            cout << *(--arr.end()) << " "<<*(arr.begin()) << "\n";
+        }
+        
+    }
+    return 0;
+}
