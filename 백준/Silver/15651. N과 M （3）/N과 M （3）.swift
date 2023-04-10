f7e52c337d85f36964798be7a9f8f19b8236006f
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:52:09 +0900

    [Silver III] Title: N과 M (3), Time: 712 ms, Memory: 79168 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let N = inputs[0]
+let M = inputs[1]
+
+var arr = [String]()
+
+func dfs(cnt:Int){
+    if cnt == M{
+        if arr.count == M{
+            print(arr.joined())
+        }
+        return
+    }
+    
+    for i in 1...N{
+        arr.append(String(i)+" ")
+        dfs(cnt: cnt+1)
+        
+        arr.removeLast()
+    }
+}
+dfs(cnt: 0)
