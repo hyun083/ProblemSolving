ff3f7da423f78f7635ac8e8211ee70a5d7a1e849
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:20:48 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 3496 ms, Memory: 95904 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+var ans = ""
+while let line = readLine(){
+    let x = Int(line)! * 10000000
+    let n = Int(readLine()!)!
+    var arr = [Int]()
+    for _ in 0..<n{
+        arr.append(Int(readLine()!)!)
+    }
+    arr.sort(by: <)
+    var start = 0
+    var end = arr.count-1
+    var str = "danger\n"
+    while start<end{
+        let sum = arr[start]+arr[end]
+        if sum == x{
+            str = "yes \(arr[start]) \(arr[end])\n"
+            break
+        }
+        if sum < x{
+            start += 1
+        }
+        if sum > x{
+            end -= 1
+        }
+    }
+    ans += str
+}
+print(ans)
