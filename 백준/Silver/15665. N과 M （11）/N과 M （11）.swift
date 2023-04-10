36424218c8969b114ccd242f0bf33a6238088d23
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:53:10 +0900

    [Silver II] Title: N과 M (11), Time: 652 ms, Memory: 98428 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+import Foundation
+
+let inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let N = inputs[0]
+let M = inputs[1]
+
+var num = readLine()!.split(separator: " ").map{Int(String($0))!}
+num.sort(by: <)
+
+var arr = [String]()
+var ans = Dictionary<String,Bool>()
+var res = String()
+
+func dfs(cnt:Int){
+    if cnt == M{
+        if arr.count == M{
+            if ans[arr.joined(separator: " ")] == nil {
+                ans[arr.joined(separator: " ")] = true
+                res += arr.joined(separator: " ") + "\n"
+                return
+            }
+        }
+        return
+    }
+    
+    for i in 0..<N{
+        arr.append(String(num[i]))
+        dfs(cnt: cnt+1)
+        arr.removeLast()
+    }
+}
+
+dfs(cnt: 0)
+print(res)
