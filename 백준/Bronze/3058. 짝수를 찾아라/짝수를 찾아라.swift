60c8a35eb091786fc02952b98f98c291fa544216
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:20:02 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 20 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t{
+    var sum = 0
+    var myMin = Int.max
+    let line = readLine()!.split(separator: " ").map{Int(String($0))!}
+    for line in line{
+        if line % 2 == 0{
+            sum += line
+            myMin = min(myMin, line)
+        }
+    }
+    print(sum,myMin)
+}
