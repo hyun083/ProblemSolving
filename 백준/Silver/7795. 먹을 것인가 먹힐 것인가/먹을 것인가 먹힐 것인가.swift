796bbb994f31830c07a3ae4461a7172a96a330de
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:28:18 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 100 ms, Memory: 82056 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+for _ in 0..<Int(readLine()!)!{
+    let nm = readLine()!.split(separator: " ").map{Int($0)!}
+    let n = nm[0]
+    let m = nm[1]
+    let a = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: <)
+    let b = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: <)
+    
+    var cnt = 0
+    var ans = 0
+    var idx1 = 0
+    var idx2 = 0
+    while idx1<n || idx2<m{
+        if a[idx1] > b[idx2]{
+            cnt += 1
+            idx2 += 1
+            if idx2 >= m{
+                ans += cnt*(n-idx1)
+                break
+            }
+        }else{
+            ans += cnt
+            idx1 += 1
+            if idx1 >= n{
+                break
+            }
+        }
+    }
+    
+    print(ans)
+}
