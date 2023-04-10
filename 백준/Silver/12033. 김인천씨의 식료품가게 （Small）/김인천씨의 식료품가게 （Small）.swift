0b9ca8c03dd101a19643220e8aedeeaaa2f08d8a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:44:28 +0900

    [Silver V] Title: 김인천씨의 식료품가게 (Small), Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/12033. 김인천씨의 식료품가게 （Small）/김인천씨의 식료품가게 （Small）.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+for T in 1...Int(readLine()!)!{
+    let n = Int(readLine()!)!
+    var ans = [String]()
+    let price = readLine()!.split(separator: " ").map{Int(String($0))!}
+    var visited = Array(repeating: false, count: n*2)
+    
+    func btk(cnt: Int){
+        if cnt == n*2{
+            return
+        }
+        if visited[cnt]{
+            btk(cnt: cnt+1)
+        }else{
+            for i in cnt..<n*2{
+                if !visited[i]{
+                    if Int(Double(price[i])*0.75) == price[cnt]{
+                        visited[i] = true
+                        visited[cnt] = true
+                        ans.append(String(price[cnt]))
+                        break
+                    }
+                }
+            }
+            btk(cnt: cnt+1)
+        }
+    }
+    btk(cnt: 0)
+    print("Case #\(T): \(ans.joined(separator: " "))")
+}
