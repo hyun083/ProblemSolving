55e40fbfcde5c3a315b3c04e988b2940c5552442
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 03:05:38 +0900

    [Bronze I] Title: 바둑돌 게임, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/25592. 바둑돌 게임/바둑돌 게임.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+var sum = [0]
+for i in 1...500{
+    sum.append((i*(i+1)/2))
+}
+let target = Int(readLine()!)!
+for i in 0..<500{
+    if sum[i]>target{
+        if i%2 == 0{
+            print(0)
+        }else{
+            print(sum[i]-target)
+        }
+        break
+    }else if sum[i]==target{
+        if i%2==1{
+            print(0)
+        }else{
+            print(sum[i+1]-target)
+        }
+        break
+    }
+}
