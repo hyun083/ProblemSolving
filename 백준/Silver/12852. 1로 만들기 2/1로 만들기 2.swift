d63f90fcfb9c6d2c7a18a3ce6b8ab195fa434206
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:45:08 +0900

    [Silver I] Title: 1로 만들기 2, Time: 168 ms, Memory: 188768 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.swift
@@ -0,0 +1,34 @@
+import Foundation
+var N = Int(readLine()!)!
+
+var dp = Array(repeating: -1, count: N+1)
+var numbers = Array(repeating: -1, count: N+1)
+
+func one(number:Int) -> Int{
+    if number == 1{
+        return 0
+    }
+    if dp[number] != -1{
+        return dp[number]
+    }
+    
+    var temp = one(number: number - 1) + 1
+    numbers[number] = number-1
+    
+    if number % 3 == 0 && temp > one(number: number/3) + 1{
+        temp = one(number: number/3) + 1
+        numbers[number] = number/3
+    }
+    if number % 2 == 0 && temp > one(number: number/2) + 1{
+        temp = one(number: number/2) + 1
+        numbers[number] = number/2
+    }
+    dp[number] = temp
+    
+    return temp
+}
+print(one(number: N))
+while N != -1{
+    print(N,terminator: " ")
+    N = numbers[N]
+}
