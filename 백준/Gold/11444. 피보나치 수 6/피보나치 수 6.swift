9226dcad72305ac73c3ec90f07215f93ee82a556
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:41:11 +0900

    [Gold II] Title: 피보나치 수 6, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var arr = Dictionary<Int,Int>()
+arr[0] = 0
+arr[1] = 1
+
+func fibonacci(n:Int) -> Int{
+    let check = n%2==0 ? true : false
+    if arr[n] == nil{
+        if check{
+            let fnM1 = fibonacci(n: n/2-1)
+            let fn = fibonacci(n: n/2)
+            arr[n] = (fn*(2*fnM1+fn))%1000000007
+        }else{
+            let fnP1 = fibonacci(n: n/2+1)
+            let fn = fibonacci(n: n/2)
+            arr[n] = ((fn*fn)+(fnP1*fnP1))%1000000007
+        }
+    }
+    return arr[n]!
+}
+print(fibonacci(n: n))
