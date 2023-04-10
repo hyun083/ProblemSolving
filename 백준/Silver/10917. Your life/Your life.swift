230275ce639c177155ff9ab9c7e3c881acd866cf
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-11 02:36:59 +0900

    [Silver I] Title: Your life, Time: 328 ms, Memory: 91228 KB -BaekjoonHub

---
+++ 백준/Silver/10917. Your life/Your life.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map{Int($0)!}
+let n = nm[0]
+let m = nm[1]
+var ans = -1
+
+var arr = Array(repeating: [Int](), count: n)
+var visited = Array(repeating: false, count: n)
+
+func bfs(from a:Int, count cnt:Int){
+    var q = [(a,cnt)]
+    var idx = 0
+    
+    while idx<q.count{
+        let curr = q[idx].0
+        let count = q[idx].1
+        
+        if curr == n-1{
+            ans = count
+            return
+        }
+        
+        for next in arr[curr]{
+            if !visited[next]{
+                visited[next] = true
+                q.append((next,count+1))
+            }
+        }
+        
+        idx += 1
+    }
+}
+
+for _ in 0..<m{
+    let info = readLine()!.split(separator: " ").map{Int($0)!-1}
+    let u = info[0]
+    let v = info[1]
+    arr[u].append(v)
+    arr[v].append(u)
+}
+bfs(from: 0, count: 0)
+print(ans)
