86d104e0e75577616d4946d7bdc1d7826a11615d
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-04-28 21:09:53 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.swift
@@ -0,0 +1,6 @@
+let output = ["NLCS": "North London Collegiate School",
+"BHA": "Branksome Hall Asia",
+"KIS": "Korea International School",
+"SJA": "St. Johnsbury Academy",]
+
+print(output[readLine()!]!)
