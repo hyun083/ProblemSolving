e368081b038d89ea00ef4413ec8dfc53d2eeaf62
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-05-29 21:44:22 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

--- 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.swift
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.swift
@@ -2,7 +2,7 @@ import Foundation
 
 let N = Int(readLine()!)!
 var arr = [(country:Int, student:Int, score:Int)]()
-var dic = Dictionary<Int,Int>()
+var country = Array(repeating: 0, count: 101)
 
 for _ in 0..<N{
     let info = readLine()!.split(separator: " ").map{Int($0)!}
@@ -11,16 +11,15 @@ for _ in 0..<N{
     let score = info[2]
     
     arr.append((C,S,score))
-    if dic[C] == nil{ dic[C] = 0 }
 }
 arr.sort(by: {$0.score > $1.score})
 
 var prize = 1
 for info in arr{
-    if dic[info.country]! >= 2{
+    if country[info.country] >= 2{
         continue
     }else{
-        dic[info.country]! += 1
+        country[info.country] += 1
         print(info.country, info.student)
         prize += 1
     }
