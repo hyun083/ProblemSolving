6119998623ce191403dd80824ce1a467f75dfcaf
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-07-03 21:35:47 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 12 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+switch readLine()!{
+case "SONGDO":
+    print("HIGHSCHOOL")
+case "CODE":
+    print("MASTER")
+case "2023":
+    print("0611")
+case "ALGORITHM":
+    print("CONTEST")
+default:
+    break
+}
