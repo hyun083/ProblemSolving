f0aedb3c73585f3db3e983f4aa85aee7e4d6e304
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-07-21 17:27:03 +0900

    [Platinum V] Title: 행성 터널, Time: 376 ms, Memory: 115396 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.swift
+++ 백준/Platinum/2887. 행성 터널/행성 터널.swift
@@ -7,29 +7,9 @@ struct Planet{
     let num:Int
 }
 
-func cost(a:Planet,with b:Planet) -> Int{
+func cost(_ a:Planet,with b:Planet) -> Int{
     min(min(abs(a.x-b.x), abs(a.y-b.y)), abs(a.z-b.z))
 }
-
-func readArr() -> [Int]{
-    readLine()!.split(separator: " ").map{Int($0)!}
-}
-
-let N = Int(readLine()!)!
-var planets = [Planet]()
-var arr = Array(repeating: -1, count: N)
-var ans = 0
-var component = N
-
-for num in 0..<N{
-    let info = readArr()
-    let planet = Planet(x: info[0], y: info[1], z: info[2], num: num)
-    planets.append(planet)
-}
-
-var X = planets.sorted(by: {$0.x < $1.x})
-var Y = planets.sorted(by: {$0.y < $1.y})
-var Z = planets.sorted(by: {$0.z < $1.z})
 
 func root(of node:Int) -> Int{
     if arr[node]<0{
@@ -51,20 +31,32 @@ func union(a:Int, b:Int, cost:Int){
     component -= 1
 }
 
-var xIdx = 0
-var yIdx = 0
-var zIdx = 0
+let N = Int(readLine()!)!
+var planets = [Planet]()
+var arr = Array(repeating: -1, count: N)
+var ans = 0
+var component = N
+
+for num in 0..<N{
+    let info = readLine()!.split(separator: " ").map{Int($0)!}
+    let planet = Planet(x: info[0], y: info[1], z: info[2], num: num)
+    planets.append(planet)
+}
+
+var X = planets.sorted(by: {$0.x < $1.x})
+var Y = planets.sorted(by: {$0.y < $1.y})
+var Z = planets.sorted(by: {$0.z < $1.z})
 
 var edges = [(u:Int,v:Int,cost:Int)]()
 
 for idx in 0..<N-1{
-    let xCost = cost(a: X[idx], with: X[idx+1])
-    let yCost = cost(a: Y[idx], with: Y[idx+1])
-    let zCost = cost(a: Z[idx], with: Z[idx+1])
+    let xCost = cost(X[idx], with: X[idx+1])
+    let yCost = cost(Y[idx], with: Y[idx+1])
+    let zCost = cost(Z[idx], with: Z[idx+1])
     
-    edges.append((X[idx].num,X[idx+1].num,xCost))
-    edges.append((Y[idx].num,Y[idx+1].num,yCost))
-    edges.append((Z[idx].num,Z[idx+1].num,zCost))
+    edges.append((X[idx].num, X[idx+1].num, xCost))
+    edges.append((Y[idx].num, Y[idx+1].num, yCost))
+    edges.append((Z[idx].num, Z[idx+1].num, zCost))
 }
 edges.sort(by: {$0.cost > $1.cost})
 
