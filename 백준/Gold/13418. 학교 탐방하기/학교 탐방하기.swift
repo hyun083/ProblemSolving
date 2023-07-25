00d7172af964acdae6af5ba451dfa3866fb5f6a5
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-07-25 18:34:28 +0900

    [Gold III] Title: 학교 탐방하기, Time: 784 ms, Memory: 121348 KB -BaekjoonHub

--- 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.swift
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.swift
@@ -5,9 +5,7 @@ func readInt() -> [Int]{
 }
 
 func root(of node:Int) -> Int{
-    if arr[node] < 0{
-        return node
-    }
+    if arr[node] < 0{ return node }
     
     arr[node] = root(of: arr[node])
     return arr[node]
@@ -17,7 +15,7 @@ func union(a:Int, b:Int, cost:Int) -> Int{
     let rootA = root(of: a)
     let rootB = root(of: b)
     
-    if rootA == rootB{ return 0}
+    if rootA == rootB{ return 0 }
     
     arr[rootB] = rootA
     cnt -= 1
@@ -31,14 +29,14 @@ let M = NM[1]
 var arr = Array(repeating: -1, count: N)
 var visited = Array(repeating: false, count: N)
 var cnt = N
-var ans = 0
 
 let UVC = readInt()
 let u = UVC[0]
 let v = UVC[1]
-let c = UVC[2]==0 ? 1:0
+let initialCost = union(a: u, b: v, cost: UVC[2]==0 ? 1:0)
 
-ans += union(a: u, b: v, cost: c)
+var ascCost = initialCost
+var descCost = initialCost
 
 var edges = [(u:Int,v:Int,cost:Int)]()
 
@@ -54,22 +52,18 @@ var desc = edges.sorted(by: {$0.cost > $1.cost})
 var asc = edges.sorted(by: {$0.cost < $1.cost})
 
 let tmpArr = arr
-let tmpAns = ans
 
 while cnt > 1{
     let edge = asc.removeLast()
-    ans += union(a: edge.u, b: edge.v, cost: edge.cost)
+    ascCost += union(a: edge.u, b: edge.v, cost: edge.cost)
 }
-
-let maximum = ans*ans
 
 arr = tmpArr
 cnt = N-1
-ans = tmpAns
+
 while cnt > 1{
     let edge = desc.removeLast()
-    ans += union(a: edge.u, b: edge.v, cost: edge.cost)
+    descCost += union(a: edge.u, b: edge.v, cost: edge.cost)
 }
-let minimum = ans*ans
 
-print(maximum-minimum)
+print((ascCost*ascCost) - (descCost*descCost))
