96315b82f67ced043618a67f7cff4523ed4fe98c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-10-11 18:08:47 +0900

    [Platinum III] Title: 레드 블루 스패닝 트리, Time: 760 ms, Memory: 130840 KB -BaekjoonHub

--- 백준/Platinum/4792. 레드 블루 스패닝 트리/레드 블루 스패닝 트리.swift
+++ 백준/Platinum/4792. 레드 블루 스패닝 트리/레드 블루 스패닝 트리.swift
@@ -1,7 +1,58 @@
 import Foundation
 
-while let input = readLine(){
-    let (N,M,K) = [input.split(separator: " ").map{Int($0)!}].map{($0[0], $0[1], $0[2])}[0]
+//by rhyno
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+}
+
+let file = FileIO()
+
+while true{
+    let N = file.readInt()
+    let M = file.readInt()
+    let K = file.readInt()
+    
     if N+M+K == 0{ break }
     
     var arr = Array(repeating: -1, count: N)
@@ -29,7 +80,9 @@ while let input = readLine(){
     }
     
     for _ in 0..<M{
-        let (c,u,v) = [readLine()!.split(separator: " ").map{String($0)}].map{($0[0], Int($0[1])!-1, Int($0[2])!-1)}[0]
+        let c = file.readString()
+        let u = file.readInt()-1
+        let v = file.readInt()-1
         edges.append((u,v,c))
     }
     var blueEdges = edges.sorted(by: {$0.color > $1.color})
@@ -53,6 +106,5 @@ while let input = readLine(){
     }
     let myMin = ans
     
-//    print(ans==K&&comp==1 ? 1:0)
     print(myMin<=K&&K<=myMax ? 1:0)
 }
