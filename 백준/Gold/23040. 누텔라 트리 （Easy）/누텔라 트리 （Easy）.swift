c025ff7b94d4c7818d52963b96197e26ad750059
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-11-10 16:41:15 +0900

    [Gold III] Title: 누텔라 트리 (Easy), Time: 100 ms, Memory: 94720 KB -BaekjoonHub

---
+++ 백준/Gold/23040. 누텔라 트리 （Easy）/누텔라 트리 （Easy）.swift
@@ -0,0 +1,111 @@
+import Foundation
+
+// by rhyno
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+}
+
+let file = FileIO()
+let N = file.readInt()
+var ans = 0
+
+var arr = Array(repeating: -1, count: N)
+var map = Array(repeating: [Int](), count: N)
+var visited = Array(repeating: false, count: N)
+var numberOfBlackFrom = Array(repeating: 0, count: N)
+
+func root(of node:Int) -> Int{
+    if arr[node] < 0 { return node }
+    arr[node] = root(of: arr[node])
+    return arr[node]
+}
+
+func union(a:Int, b:Int) {
+    let A = root(of: a)
+    let B = root(of: b)
+    
+    if A==B {return}
+    arr[A] += arr[B]
+    arr[B] = A
+}
+
+for _ in 0..<N-1{
+    let u = file.readInt()-1
+    let v = file.readInt()-1
+
+    map[u].append(v)
+    map[v].append(u)
+}
+
+var colorOf = file.readString().map{String($0)}
+
+func bfs(from vertex:Int){
+    var q = [vertex]
+    var cnt = 0
+    var idx = 0
+    
+    while idx < q.count{
+        let curr = q[idx]
+        for next in map[curr]{
+            if colorOf[next] == "R" && !visited[next]{
+                visited[next] = true
+                union(a: curr, b: next)
+                q.append(next)
+            }else if colorOf[next] == "B"{
+                cnt += 1
+            }
+        }
+        idx += 1
+    }
+    numberOfBlackFrom[root(of: vertex)] = cnt
+}
+
+for i in 0..<N{
+    if colorOf[i] == "R" && !visited[i]{
+        visited[i] = true
+        bfs(from: i)
+        ans += numberOfBlackFrom[i] * abs(arr[root(of: i)])
+    }
+}
+print(ans)
