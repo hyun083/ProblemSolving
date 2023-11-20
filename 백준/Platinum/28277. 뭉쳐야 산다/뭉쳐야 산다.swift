979d2850ce7804e2a9583e8e8599bcf7bad7b88a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2023-11-20 16:32:20 +0900

    [Platinum V] Title: 뭉쳐야 산다, Time: 652 ms, Memory: 145352 KB -BaekjoonHub

---
+++ 백준/Platinum/28277. 뭉쳐야 산다/뭉쳐야 산다.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+// by rhyno
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+let file = FileIO()
+let N = file.readInt()
+let Q = file.readInt()
+var arr = [Set<Int>]()
+
+for _ in 0..<N{
+    let count = file.readInt()
+    var tmp = Set<Int>()
+    for _ in 0..<count{
+        tmp.insert(file.readInt())
+    }
+    arr.append(tmp)
+}
+
+for _ in 0..<Q{
+    if file.readInt() == 1{
+        let a = file.readInt()-1
+        let b = file.readInt()-1
+        if arr[a].count < arr[b].count{
+            arr[b].formUnion(arr[a])
+            arr.swapAt(a, b)
+        }else{
+            arr[a].formUnion(arr[b])
+        }
+        arr[b].removeAll()
+    }else{
+        let a = file.readInt()-1
+        print(arr[a].count)
+    }
+}
