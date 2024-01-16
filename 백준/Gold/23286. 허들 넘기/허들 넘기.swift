9090b8ec641d94d00c56d8ff0af29c47589bbb2a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-01-16 17:35:17 +0900

    [Gold III] Title: 허들 넘기, Time: 160 ms, Memory: 80368 KB -BaekjoonHub

--- 백준/Gold/23286. 허들 넘기/허들 넘기.swift
+++ 백준/Gold/23286. 허들 넘기/허들 넘기.swift
@@ -1,85 +1,5 @@
 import Foundation
 
-//by rhyno
-public struct Heap<T> {
-  var nodes: [T] = []
-  let comparer: (T,T) -> Bool
-
-  var isEmpty: Bool {
-      return nodes.isEmpty
-  }
-
-  init(comparer: @escaping (T,T) -> Bool) {
-      self.comparer = comparer
-  }
-
-  func peek() -> T? {
-      return nodes.first
-  }
-
-  mutating func insert(_ element: T) {
-      var index = nodes.count
-
-      nodes.append(element)
-
-      while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
-          nodes.swapAt(index, (index-1)/2)
-          index = (index-1)/2
-      }
-  }
-
-  mutating func delete() -> T? {
-      guard !nodes.isEmpty else {
-          return nil
-      }
-
-      if nodes.count == 1 {
-          return nodes.removeFirst()
-      }
-
-      let result = nodes.first
-      nodes.swapAt(0, nodes.count-1)
-      _ = nodes.popLast()
-
-      var index = 0
-
-      while index < nodes.count {
-          let left = index * 2 + 1
-          let right = left + 1
-
-          if right < nodes.count {
-              if comparer(nodes[left], nodes[right]),
-                  !comparer(nodes[right], nodes[index]) {
-                  nodes.swapAt(right, index)
-                  index = right
-              } else if !comparer(nodes[left], nodes[index]){
-                  nodes.swapAt(left, index)
-                  index = left
-              } else {
-                  break
-              }
-          } else if left < nodes.count {
-              if !comparer(nodes[left], nodes[index]) {
-                  nodes.swapAt(left, index)
-                  index = left
-              } else {
-                  break
-              }
-          } else {
-              break
-          }
-      }
-
-      return result
-  }
-}
-
-extension Heap where T: Comparable {
-    init() {
-        self.init(comparer: <)
-    }
-}
-
 let (N,M,T) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0], $0[1], $0[2])}[0]
 var map = Array(repeating: [(node:Int, cost:Int)](), count:N)
 
@@ -90,23 +10,30 @@ for _ in 0..<M{
 
 func dijk(from root:Int) -> [Int]{
     var minCost = Array(repeating: -1, count: N)
-    var q = Heap<(node:Int, cost:Int)>(comparer: {$0.cost > $1.cost})
+    var q = [(node:Int, cost:Int)]()
+    var dq = q
     
     minCost[root] = 0
-    q.insert((root,0))
+    q.append((root,0))
     
     while !q.isEmpty{
-        let curr = q.delete()!
-        if minCost[curr.node] < curr.cost{ continue }
+        dq = q.reversed()
+        q.removeAll()
         
-        for next in map[curr.node]{
-            let newCost = max(curr.cost, next.cost)
+        for _ in 0..<dq.count{
+            let curr = dq.removeLast()
+            if minCost[curr.node] < curr.cost{ continue }
             
-            if minCost[next.node]<0 || minCost[next.node]>newCost{
-                minCost[next.node] = newCost
-                q.insert((next.node, newCost))
+            for next in map[curr.node]{
+                let newCost = max(curr.cost, next.cost)
+                
+                if minCost[next.node]<0 || minCost[next.node]>newCost{
+                    minCost[next.node] = newCost
+                    q.append((next.node, newCost))
+                }
             }
         }
+        q.sort(by: {$0.cost < $1.cost})
     }
     
     return minCost
