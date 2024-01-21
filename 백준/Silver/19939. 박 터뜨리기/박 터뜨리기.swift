882a92cb416110bb0b15553893ee26d574835ea9
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-01-21 20:44:59 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.swift
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.swift
@@ -2,9 +2,4 @@ import Foundation
 
 let (N,K) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0], $0[1])}[0]
 let res = K*(K+1)/2
-
-if res > N{
-    print(-1)
-}else if res <= N{
-    print((N-res)%K == 0 ? K-1:K)
-}
+print(N<res ? -1:(N-res)%K == 0 ? K-1:K)
