d92c7fe35a78849013c9a811a466a26d34be6df3
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-01-23 16:32:56 +0900

    [Gold V] Title: 전구와 스위치, Time: 32 ms, Memory: 81872 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.swift
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.swift
@@ -5,32 +5,33 @@ var arr = readLine()!.map{Int(String($0))!}
 var target = readLine()!.map{Int(String($0))!}
 
 var curr = arr
-var ans = -1
+var ans = N+1
 var cnt = 0
 
-for i in 1..<N{
-    if curr[i-1]==target[i-1]{ continue }
-    curr[i-1]+=1; curr[i-1]%=2
-    curr[i]+=1; curr[i]%=2
-    if i+1<N { curr[i+1]+=1; curr[i+1]%=2 }
+func toggle(_ num:Int){
+    let dx = [-1,0,1]
+    
+    for i in 0..<3{
+        let nx = num+dx[i]
+        if nx<0 || nx>=N { continue }
+        curr[nx] += 1
+        curr[nx] %= 2
+    }
     cnt += 1
 }
 
-if curr == target { ans = cnt }
+for i in 1..<N{
+    if curr[i-1]==target[i-1]{ continue }
+    toggle(i)
+}
+ans = curr==target ? cnt:ans
+
 curr = arr
-cnt = 1
-
-curr[0]+=1; curr[0]%=2
-curr[1]+=1; curr[1]%=2
+cnt = 0
+toggle(0)
 for i in 1..<N{
     if curr[i-1]==target[i-1]{ continue }
-    curr[i-1]+=1; curr[i-1]%=2
-    curr[i]+=1; curr[i]%=2
-    if i+1<N { curr[i+1]+=1; curr[i+1]%=2 }
-    cnt += 1
+    toggle(i)
 }
-
-if curr==target{
-    ans = ans<0 ? cnt:min(ans,cnt)
-}
-print(ans)
+ans = curr==target ? min(ans,cnt):ans
+print(ans==N+1 ? -1:ans)
