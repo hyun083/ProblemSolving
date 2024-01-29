38f04936ee9806b11394f45bed81ac7228ae686a
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-01-29 19:07:14 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 1832 ms, Memory: 154284 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.swift
@@ -0,0 +1,99 @@
+import Foundation
+
+//by rhyno
+public struct Heap<T> {
+  var nodes: [T] = []
+  let comparer: (T,T) -> Bool
+
+  var isEmpty: Bool {
+      return nodes.isEmpty
+  }
+
+  init(comparer: @escaping (T,T) -> Bool) {
+      self.comparer = comparer
+  }
+
+  func peek() -> T? {
+      return nodes.first
+  }
+
+  mutating func insert(_ element: T) {
+      var index = nodes.count
+
+      nodes.append(element)
+
+      while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
+          nodes.swapAt(index, (index-1)/2)
+          index = (index-1)/2
+      }
+  }
+
+  mutating func delete() -> T? {
+      guard !nodes.isEmpty else {
+          return nil
+      }
+
+      if nodes.count == 1 {
+          return nodes.removeFirst()
+      }
+
+      let result = nodes.first
+      nodes.swapAt(0, nodes.count-1)
+      _ = nodes.popLast()
+
+      var index = 0
+
+      while index < nodes.count {
+          let left = index * 2 + 1
+          let right = left + 1
+
+          if right < nodes.count {
+              if comparer(nodes[left], nodes[right]),
+                  !comparer(nodes[right], nodes[index]) {
+                  nodes.swapAt(right, index)
+                  index = right
+              } else if !comparer(nodes[left], nodes[index]){
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else if left < nodes.count {
+              if !comparer(nodes[left], nodes[index]) {
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else {
+              break
+          }
+      }
+
+      return result
+  }
+}
+
+extension Heap where T: Comparable {
+    init() {
+        self.init(comparer: <)
+    }
+}
+
+let T = Int(readLine()!)!
+
+for _ in 0..<T{
+    let _ = Int(readLine()!)!
+    let arr = readLine()!.split(separator: " ").map{Int($0)!}
+    var heap = Heap<Int>(comparer: >)
+    var ans = 0
+    
+    for file in arr{ heap.insert(file) }
+    
+    while heap.nodes.count > 1{
+        let tmp = heap.delete()! + heap.delete()!
+        ans += tmp
+        heap.insert(tmp)
+    }
+    print(ans)
+}
