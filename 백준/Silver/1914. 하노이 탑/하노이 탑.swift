91a7933ee62cc2f5fc94aaa8a21080b772317e2c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-02-06 17:52:34 +0900

    [Silver I] Title: 하노이 탑, Time: 340 ms, Memory: 111008 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.swift
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 let N = Int(readLine()!)!
+var ans = [String]()
 
 func move(plate:Int,from:Int, by:Int, to:Int){
     if plate == 1{
-        print(from,to)
+        ans.append("\(from) \(to)")
     }else{
         move(plate: plate-1, from: from, by: to, to: by)
-        print(from,to)
+        ans.append("\(from) \(to)")
         move(plate: plate-1, from: by, by: from, to: to)
     }
 }
@@ -15,6 +16,7 @@ func move(plate:Int,from:Int, by:Int, to:Int){
 if N <= 20{
     print(pow(2,N)-1)
     move(plate: N, from: 1, by: 2, to: 3)
+    print(ans.joined(separator: "\n"))
 }else{
     print(pow(2, N)-1)
 }
