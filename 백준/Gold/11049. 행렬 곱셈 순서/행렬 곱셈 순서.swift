e1202585471c8af26755dc93f181ab302659a6d9
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-02-08 17:59:56 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 544 ms, Memory: 81496 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let N = Int(readLine()!)!
+var arr = [(N:Int, M:Int)]()
+let INF = Int.max
+var dp = Array(repeating: Array(repeating: 0, count: N), count: N)
+
+for _ in 0..<N{
+    let (n,m) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0], $0[1])}[0]
+    arr.append((n,m))
+}
+
+for i in 0..<N-1{
+    let value = arr[i].N * arr[i].M * arr[i+1].M
+    dp[i][i+1] = value
+}
+
+if N>1{
+    for range in 1..<N{
+        for i in 0..<N-range{
+            dp[i][i+range] = INF
+            for k in i..<i+range{
+                dp[i][i+range] = min(dp[i][i+range], dp[i][k]+dp[k+1][i+range] + (arr[i].N * arr[k].M * arr[i+range].M))
+            }
+        }
+    }
+    print(dp[0][N-1])
+}else{
+    print(0)
+}
