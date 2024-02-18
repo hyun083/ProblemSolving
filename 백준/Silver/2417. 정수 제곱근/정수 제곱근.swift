53bd7495b297eccc5f7f91f322526f67e0b148bb
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-02-18 15:47:40 +0900

    [Silver IV] Title: 정수 제곱근, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let N = UInt(readLine()!)!
+var lo:UInt = 0
+var hi = UInt(truncating: NSDecimalNumber(decimal: pow(2, 32)))-1
+
+while lo+1<hi{
+    let mid:UInt = (lo+hi)/2
+    if mid*mid < N{
+        lo = mid+1
+    }else{
+        hi = mid
+    }
+}
+if lo*lo < N{
+    lo += 1
+}
+print(lo)
