1cd1395ee6d65cc17155df685e48a8a87e5c0475
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-03-15 22:50:48 +0900

    [Gold IV] Title: Convention, Time: 68 ms, Memory: 87476 KB -BaekjoonHub

---
+++ 백준/Gold/16766. Convention/Convention.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+let (N,M,C) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0], $0[1], $0[2])}[0]
+var arr = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: <)
+var lo = 0
+var hi = arr.max()!*2
+var ans = Int.max
+
+while lo<=hi{
+    let mid = (lo+hi)/2
+    var bus = 0
+    var cnt = 1
+    var pre = arr[0]
+    var res = 0
+    
+    for i in 1..<N{
+        if arr[i]-pre > mid || cnt+1>C{
+            res = max(res, arr[i-1]-pre)
+            pre = arr[i]
+            cnt = 1
+            bus += 1
+            if i==N-1{
+                res = max(res, arr.last!-pre)
+                bus += 1
+            }
+        }else{
+            cnt += 1
+            if i==N-1{
+                res = max(res, arr.last!-pre)
+                bus += 1
+            }
+        }
+    }
+    
+    if bus<=M {
+        ans = min(res, ans)
+        hi = mid-1
+    }else{
+        lo = mid+1
+    }
+}
+print(ans)
