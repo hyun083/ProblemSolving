123ed032ae2c0f0e71317d2419965592d2965128
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-03-21 21:20:17 +0900

    [Bronze IV] Title: 모비스, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.swift
@@ -0,0 +1,7 @@
+let S = Set(readLine()!.map{String($0)})
+
+if S.contains("M") && S.contains("O") && S.contains("B") && S.contains("I") && S.contains("S"){
+    print("YES")
+}else{
+    print("NO")
+}
