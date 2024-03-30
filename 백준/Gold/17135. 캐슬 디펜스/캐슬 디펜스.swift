b0c6f523e96b5781ca64aed03a01c1eb12eb8d1e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-03-30 19:04:04 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 68 ms, Memory: 79528 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.swift
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.swift
@@ -8,48 +8,37 @@ for _ in 0..<N{
     map.append(readLine()!.split(separator: " ").map{Int($0)!})
 }
 
-func makeSoldier() -> [[(x:Int,y:Int)]]{
-    var res = [[(x:Int, y:Int)]]()
-    
-    for i in 0..<M-2{
-        for k in i+1..<M-1{
-            for p in k+1..<M{
-                res.append([(N,i),(N,k),(N,p)])
-            }
+var S = [[(x:Int, y:Int)]]()
+for i in 0..<M-2{
+    for k in i+1..<M-1{
+        for p in k+1..<M{
+            S.append([(N,i),(N,k),(N,p)])
         }
     }
-                            
-    return res
 }
 
-func target(x:Int, y:Int, d:Int, mp:[[Int]]) -> (x:Int, y:Int){
+func target(x:Int, y:Int, mp:[[Int]]) -> (x:Int, y:Int){
     var res = [(x:Int,y:Int,d:Int)]()
     
-    for nx in (x-d..<x).reversed(){
-        for ny in (y-d..<y).reversed(){
+    for nx in (x-D..<x).reversed(){
+        for ny in y-D..<y+D{
+            let distance = abs(x-nx) + abs(y-ny)
             if nx<0 || ny<0 || ny>=M{ continue }
-            if abs(x-nx) + abs(y-ny) > d { continue }
+            if  distance > D { continue }
+            
             if mp[nx][ny]==1{
-                res.append((nx,ny,abs(x-nx) + abs(y-ny)))
-            }
-        }
-        for ny in y..<y+d{
-            if nx<0 || ny<0 || ny>=M{ continue }
-            if abs(x-nx) + abs(y-ny) > d { continue }
-            if mp[nx][ny]==1{
-                res.append((nx,ny,abs(x-nx) + abs(y-ny)))
+                res.append((nx,ny,distance))
             }
         }
     }
-    res.sort(by: {$0.d==$1.d ? $0.y<$1.y:$0.d<$1.d})
+    
     if res.isEmpty{
         return (-1,-1)
     }else{
+        res.sort(by: {$0.d==$1.d ? $0.y<$1.y:$0.d<$1.d})
         return (res[0].x,res[0].y)
     }
 }
-
-var S = makeSoldier()
 
 while !S.isEmpty{
     var s = S.removeLast()
@@ -60,7 +49,7 @@ while !S.isEmpty{
         var res = Set<[Int]>()
         
         for i in 0..<3{
-            let T = target(x: s[i].x, y: s[i].y, d: D, mp: m)
+            let T = target(x: s[i].x, y: s[i].y, mp: m)
             res.insert([T.x,T.y])
             s[i].x -= 1
         }
