1326f206d4f42ab5ffb27cc565a2a97fe3dadc09
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-04-21 17:25:13 +0900

    [Gold IV] Title: 미로 탈출, Time: 216 ms, Memory: 134944 KB -BaekjoonHub

---
+++ 백준/Gold/14923. 미로 탈출/미로 탈출.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+let (N,M) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0], $0[1])}[0]
+let (Hx,Hy) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0]-1, $0[1]-1)}[0]
+let (Ex,Ey) = [readLine()!.split(separator: " ").map{Int($0)!}].map{($0[0]-1, $0[1]-1)}[0]
+var ans = -1
+
+var map = [[Int]]()
+var visited = Array(repeating: Array(repeating: Array(repeating: false, count: M), count: N), count: 2)
+
+for _ in 0..<N{
+    let line = readLine()!.split(separator: " ").map{Int($0)!}
+    map.append(line)
+}
+
+func bfs(){
+    let dx = [-1,1,0,0]
+    let dy = [0,0,-1,1]
+    
+    var q = [(x:Int, y:Int, wand:Bool, cnt:Int)]()
+    var idx = 0
+    
+    q.append((Hx,Hy,false,0))
+    while idx < q.count{
+        let curr = q[idx]
+        if curr.x == Ex && curr.y == Ey {
+            ans = curr.cnt
+            return
+        }
+        
+        for i in 0..<4{
+            let nx = curr.x + dx[i]
+            let ny = curr.y + dy[i]
+            
+            if nx<0 || nx>=N || ny<0 || ny>=M { continue }
+            if curr.wand{
+                if map[nx][ny]==0 && !visited[1][nx][ny]{
+                    visited[1][nx][ny] = true
+                    q.append((nx,ny,curr.wand,curr.cnt+1))
+                }
+            }else{
+                if map[nx][ny]==1 && !visited[1][nx][ny]{
+                    visited[1][nx][ny] = true
+                    q.append((nx,ny,true,curr.cnt+1))
+                }else if map[nx][ny]==0 && !visited[0][nx][ny]{
+                    visited[0][nx][ny] = true
+                    q.append((nx,ny,curr.wand,curr.cnt+1))
+                }
+            }
+        }
+        
+        idx += 1
+    }
+}
+
+bfs()
+print(ans)
