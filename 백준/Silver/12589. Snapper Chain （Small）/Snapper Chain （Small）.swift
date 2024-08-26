75629cf9043b5aa30d61a86866eeebdb718525d0
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-08-26 21:02:33 +0900

    [Silver V] Title: Snapper Chain (Small), Time: 36 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/12589. Snapper Chain （Small）/Snapper Chain （Small）.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let T = Int(readLine()!)!
+for t in 1...T{
+    let NK = readLine()!.split(separator: " ").map{Int($0)!}
+    let N = NK[0]
+    let K = NK[1]
+    var tmp = 0
+    
+    var arr = Array(repeating: false, count: N)
+    for _ in 0..<K{
+        for i in (1..<N).reversed(){
+            if arr[0...i-1].reduce(true, {$0 && $1}){
+                arr[i].toggle()
+            }
+        }
+        arr[0].toggle()
+    }
+    let ans = arr.reduce(true, {$0 && $1}) ? "ON":"OFF"
+    print("Case #\(t): \(ans)")
+}
