e5365cbfc92d349f84266f00ed25678c6e5f880e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-11-01 22:33:39 +0900

    [Silver V] Title: 아니 이게 왜 안 돼, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/29812. 아니 이게 왜 안 돼/아니 이게 왜 안 돼.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int N,D,M;
+    int H=0, Y=0, U=0;
+    int cost = 0;
+    cin >> N;
+    vector<char> arr;
+    for(int i=0; i<N; i++){
+        char tmp;
+        cin >> tmp;
+        H += tmp=='H' ? 1:0;
+        Y += tmp=='Y' ? 1:0;
+        U += tmp=='U' ? 1:0;
+        arr.push_back(tmp);
+    }
+    cin >> D >> M;
+    int cnt = 0;
+    for(auto data: arr){
+        if(data=='H' || data=='Y' || data=='U'){
+            if(cnt>0){
+                if(cnt==1){
+                    cost += D;
+                }else{
+                    cost += min(D*cnt,D+M);
+                }
+            }
+            cnt = 0;
+        }else{
+            cnt++;
+        }
+    }
+    if(cnt>0){
+        if(cnt==1){
+            cost += D;
+        }else{
+            cost += min(D*cnt,D+M);
+        }
+    }
+    if(cost==0){
+        cout << "Nalmeok" << "\n";
+    }else{
+        cout << cost << "\n";
+    }
+    
+    if(min(H,min(Y,U)) == 0){
+        cout << "I love HanYang University" << "\n";
+    }else{
+        cout << min(H,min(Y,U)) << "\n";
+    }
+    return 0;
+}
