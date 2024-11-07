9adf1c168eed8944a4835bf1ded073d4f7f37e08
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-11-07 20:50:21 +0900

    [Silver IV] Title: 힘 겨루기, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/16218. 힘 겨루기/힘 겨루기.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+let NK = readLine()!.split(separator: " ").map{Int($0)!}
+let N = NK[0]
+let K = NK[1]
+var sumA = 0
+var sumB = 0
+var A = [Int]()
+var B = [Int]()
+var overpower = false
+var ans = 0
+
+for _ in 0..<N{
+    let AB = readLine()!.split(separator: " ").map{Int($0)!}
+    let a = AB[0]
+    let b = AB[1]
+    sumA += a
+    sumB += b
+    if !overpower && (sumA+(a/2)>=K && sumB<K) || (sumA+(a/2) >= sumB+50){
+        sumA += (a+(a/2))
+        overpower = true
+    }
+    if sumA >= sumB+50{
+        ans = 1
+        break
+    }else if (sumA>=K || sumB>=K){
+        if sumA>=K && sumB>=K{
+            if overpower{ ans = -1}
+            else{ ans = 1 }
+        }else if sumA>=K{
+            ans = 1
+        }else {
+            ans = -1
+        }
+        break
+    }
+}
+print(ans)
