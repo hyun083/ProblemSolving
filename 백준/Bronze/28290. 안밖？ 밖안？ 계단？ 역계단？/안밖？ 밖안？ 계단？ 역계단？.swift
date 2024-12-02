28e40d2c194ee9b8c01e481ed392a0e0785dfa5c
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2024-12-02 23:27:58 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.swift
@@ -0,0 +1,12 @@
+let S = readLine()!
+if S == "fdsajkl;" || S == "jkl;fdsa"{
+    print("in-out")
+}else if S == "asdf;lkj" || S == ";lkjasdf"{
+    print("out-in")
+}else if S == "asdfjkl;"{
+    print("stairs")
+}else if S == ";lkjfdsa"{
+    print("reverse")
+}else{
+    print("molu")
+}
