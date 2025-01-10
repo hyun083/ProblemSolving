c4e790d94d8406bce9d21880d97d50ee442b6d93
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-01-10 18:56:47 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+let score = [12,11,11,10,9,9,9,8,7,6,6]
+let panelty = [1600,894,1327,1311,1004,1178,1357,837,1055,556,773]
+let N = Int(readLine()!)!-1
+print(score[N],panelty[N])
