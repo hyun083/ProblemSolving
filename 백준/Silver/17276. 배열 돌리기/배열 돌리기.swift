93b50fb391342cc67421fd4edb83d07b4a56af24
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-01-18 18:02:48 +0900

    [Silver I] Title: 배열 돌리기, Time: 732 ms, Memory: 87276 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.swift
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.swift
@@ -5,80 +5,62 @@ let T = Int(readLine()!)!
 for _ in 0..<T{
     let ND = readLine()!.split(separator: " ").map{Int($0)!}
     let N = ND[0]
-    var D = ND[1]
-    var arr = [[Int]]()
-    var tmp = [[Int]]()
+    let D = ND[1]
+    var arr = [[String]]()
     
     for _ in 0..<N{
-        let info = readLine()!.split(separator: " ").map{Int($0)!}
+        let info = readLine()!.split(separator: " ").map{String($0)}
         arr.append(info)
     }
     
     func clockWise(){
+        var tmp = [[String]]()
         tmp = arr
-        //diagnal -> vertical
         for i in 0..<N{
+            //diagnal -> vertical
             tmp[i][N/2] = arr[i][i]
-        }
-        
-        //horizontal -> diagnal
-        for k in 0..<N{
-            tmp[k][k] = arr[N/2][k]
-        }
-        
-        //diagnal -> horizontal
-        for i in 0..<N{
+            
+            //horizontal -> diagnal
+            tmp[i][i] = arr[N/2][i]
+            
+            //diagnal -> horizontal
             tmp[N/2][N-1-i] = arr[i][N-1-i]
-        }
-        
-        //vertical -> diagnal
-        for i in 0..<N{
+            
+            //vertical -> diagnal
             tmp[i][N-1-i] = arr[i][N/2]
         }
         arr = tmp
     }
     
     func antiClockWise(){
+        var tmp = [[String]]()
         tmp = arr
-        //diagnal <- vertical
         for i in 0..<N{
-            let k = N/2
-            tmp[i][i] = arr[i][k]
-        }
-        
-        //horizontal <= diagnal
-        for k in 0..<N{
-            let i = N/2
-            tmp[i][k] = arr[k][k]
-        }
-        
-        //diagnal <- horizontal
-        for i in 0..<N{
+            //diagnal <- vertical
+            tmp[i][i] = arr[i][N/2]
+            
+            //horizontal <= diagnal
+            tmp[N/2][i] = arr[i][i]
+            
+            //diagnal <- horizontal
             tmp[N-1-i][i] = arr[N/2][i]
-        }
-        
-        //vertical <- diagnal
-        for i in 0..<N{
+            
+            //vertical <- diagnal
             tmp[i][N/2] = arr[i][N-1-i]
         }
         arr = tmp
     }
-     
-    if D<0{
-        D *= -1
-        for _ in 0..<D/45{
+    
+    let cnt = D/45<0 ? -1*D/45:D/45
+    for _ in 0..<cnt{
+        if D<0 {
             antiClockWise()
-        }
-    }else{
-        for _ in 0..<D/45{
+        }else{
             clockWise()
         }
     }
      
     for ans in arr{
-        for num in ans{
-            print(num, terminator: " ")
-        }
-        print()
+        print(ans.joined(separator: " "))
     }
 }
