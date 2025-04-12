d3492d4e43c0a66c2b7f7ca8b098c37c0a259d6d
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-04-12 20:54:40 +0900

    [Silver III] Title: 스피카, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/21316. 스피카/스피카.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+var indegree = Array(repeating: 0, count: 12)
+var map = Array(repeating: [Int](), count: 12)
+
+for _ in 0..<12{
+    let UV = readLine()!.split(separator: " ").map{Int($0)!-1}
+    let u = UV[0]
+    let v = UV[1]
+    indegree[u] += 1
+    indegree[v] += 1
+    
+    map[u].append(v)
+    map[v].append(u)
+}
+
+func check(from curr:Int) -> Bool{
+    var arr = [Int]()
+    
+    for next in map[curr]{
+        arr.append(indegree[next])
+    }
+    
+    arr.sort(by: <)
+    if arr == [1,2,3]{
+        return true
+    }else{
+        return false
+    }
+}
+
+for i in 0..<12{
+    var tmp = false
+    if indegree[i] == 3{
+        tmp = check(from: i)
+    }
+    if tmp{
+        print(i+1)
+        break
+    }
+}
