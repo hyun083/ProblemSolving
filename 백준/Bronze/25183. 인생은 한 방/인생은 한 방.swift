8cbf232c95186ff50d01087256d83c6223394869
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-07-22 22:57:24 +0900

    [Bronze I] Title: 인생은 한 방, Time: 20 ms, Memory: 80432 KB -BaekjoonHub

--- 백준/Bronze/25183. 인생은 한 방/인생은 한 방.swift
+++ 백준/Bronze/25183. 인생은 한 방/인생은 한 방.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 let N = Int(readLine()!)!
-let arr = readLine()!.map{String($0)}
+let arr = readLine()!.map{Int($0.unicodeScalars.first!.value)}
 var res = true
 for i in 0..<N-4{
-    let tmp = arr[i..<i+5].map{Int($0.unicodeScalars.first!.value)}
+    let tmp = Array(arr[i..<i+5])
     res = true
     for k in 0..<4{
         if abs(tmp[k]-tmp[k+1]) != 1{
