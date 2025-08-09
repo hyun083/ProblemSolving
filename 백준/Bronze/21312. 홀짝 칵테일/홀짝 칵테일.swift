145983ab9b91c6e9937dfc1c00588def26c24484
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-08-09 23:07:32 +0900

    [Bronze III] Title: 홀짝 칵테일, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Bronze/21312. 홀짝 칵테일/홀짝 칵테일.swift
+++ 백준/Bronze/21312. 홀짝 칵테일/홀짝 칵테일.swift
@@ -4,39 +4,11 @@ let arr = readLine()!.split(separator: " ").map{Int($0)!}
 let X = arr[0]
 let Y = arr[1]
 let Z = arr[2]
-var odd = [Int]()
-var even = [Int]()
+var ans = arr
 
-for num in arr{
-    if num%2 == 0{
-        even.append(num)
-    }else{
-        odd.append(num)
-    }
-}
-
-if (X*Y)%2 == 0{
-    even.append(X*Y)
-}else{
-    odd.append(X*Y)
-}
-
-if (X*Z)%2 == 0{
-    even.append(X*Z)
-}else{
-    odd.append(X*Z)
-}
-
-if (Y*Z)%2 == 0{
-    even.append(Y*Z)
-}else{
-    odd.append(Y*Z)
-}
-
-if (X*Y*Z)%2 == 0{
-    even.append(X*Y*Z)
-}else{
-    odd.append(X*Y*Z)
-}
-
-print(odd.max() ?? even.max()!)
+ans.append(X*Y)
+ans.append(X*Z)
+ans.append(Y*Z)
+ans.append(X*Y*Z)
+ans.sort(by:{ $0%2==$1%2 ? $0>$1 : $0%2>$1%2})
+print(ans[0])
