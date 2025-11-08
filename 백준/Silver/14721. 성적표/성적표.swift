70864313b4db655ccc2504a160a1e6b9fdb76199
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-11-08 15:16:23 +0900

    [Silver V] Title: 성적표, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/14721. 성적표/성적표.swift
+++ 백준/Silver/14721. 성적표/성적표.swift
@@ -2,8 +2,6 @@ import Foundation
 
 let N = Int(readLine()!)!
 var arr = [(x:Int,y:Int)]()
-var param = 0
-var score = 0
 
 for _ in 0..<N{
     let XY = readLine()!.split(separator: " ").map{Int($0)!}
@@ -11,9 +9,8 @@ for _ in 0..<N{
     let Y = XY[1]
     
     arr.append((X,Y))
-    param += X
-    score += Y
 }
+
 var target = Int.max
 var resA = 0
 var resB = 0
