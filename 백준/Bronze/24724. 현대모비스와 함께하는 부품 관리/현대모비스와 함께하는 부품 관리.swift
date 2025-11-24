b7a860b55508a664a4ed9735288fc64a1935f62e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-11-24 22:02:52 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 44 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let T = Int(readLine()!)!
+for num in 1...T{
+    let N = Int(readLine()!)!
+    let _ = readLine()!
+    print("Material Management \(num)")
+    for _ in 0..<N{ let _ = readLine()! }
+    print("Classification ---- End!")
+}
