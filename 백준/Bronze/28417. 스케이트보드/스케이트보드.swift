dbf9702e7808c3c1d7144c35693024135311583e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2025-12-24 23:57:37 +0900

    [Bronze III] Title: 스케이트보드, Time: 240 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Bronze/28417. 스케이트보드/스케이트보드.swift
+++ 백준/Bronze/28417. 스케이트보드/스케이트보드.swift
@@ -3,14 +3,11 @@ import Foundation
 let N = Int(readLine()!)!
 var ans = 0
 for _ in 0..<N{
-    var score:[Int] = readLine()!.split(separator: " ").map{Int($0)!}.reversed()
-    var runScore = [Int]()
-    var trickScore = [Int]()
-    for _ in 0..<2{
-        runScore.append(score.removeLast())
-    }
-    trickScore = score.sorted(by: >)
-    let res = runScore.max()! + trickScore[0] + trickScore[1]
+    let score = readLine()!.split(separator: " ").map{Int($0)!}
+    let runScore = score.prefix(2).sorted(by: >)
+    let trickScore = score.suffix(5).sorted(by: >)
+    
+    let res = runScore[0] + trickScore[0] + trickScore[1]
     ans = max(ans, res)
 }
 print(ans)
