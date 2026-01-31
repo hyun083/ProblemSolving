04f8a79457a74632cf0356aa7ca134f1b88c2769
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2026-01-31 18:29:08 +0900

    [Bronze I] Title: 배고픈 아리의 샌드위치 구매하기, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.swift
+++ 백준/Bronze/25166. 배고픈 아리의 샌드위치 구매하기/배고픈 아리의 샌드위치 구매하기.swift
@@ -1,24 +1,21 @@
 import Foundation
 
 let SM = readLine()!.split(separator: " ").map{Int($0)!}
-let S = SM[0]
-let M = SM[1]
-let remain = S-1023
-var padCount = 0
+let remain = SM[0]-1023
+let R = String(remain,radix:2).map{String($0)}
+let M = String(SM[1],radix:2).map{String($0)}
 
 if remain<=0{
     print("No thanks")
 }else{
-    let remainArr = String(remain,radix: 2).map{String($0)}
-    let mArr = String(M,radix: 2).map{String($0)}
-    let biR = Array(repeating: "0", count: max(0,11-remainArr.count))+remainArr
-    let biM = Array(repeating: "0", count: max(0,11-mArr.count))+mArr
-    var flag = true
+    let biR = Array(repeating: "0", count: max(0,11-R.count)) + R
+    let biM = Array(repeating: "0", count: max(0,11-M.count)) + M
+    var valid = true
     
     for i in 0..<11{
         if biR[i]=="1" && biM[i]=="0"{
-            flag = false
+            valid = false
         }
     }
-    print(flag ? "Thanks":"Impossible")
+    print(valid ? "Thanks":"Impossible")
 }
