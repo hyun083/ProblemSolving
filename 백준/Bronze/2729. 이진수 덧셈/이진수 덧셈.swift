d878481efb1d4aeaaff187d5bd3bd70c11ea3eb3
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2026-02-14 18:26:06 +0900

    [Bronze I] Title: 이진수 덧셈, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Bronze/2729. 이진수 덧셈/이진수 덧셈.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+let T = Int(readLine()!)!
+for _ in 0..<T{
+    let line = readLine()!.split(separator: " ").map{String($0)}
+    let len = max(line[0].count, line[1].count)
+    let A = Array(repeating: "0", count: len-line[0].count) + line[0].map{String($0)}
+    let B = Array(repeating: "0", count: len-line[1].count) + line[1].map{String($0)}
+    
+    var temp = "0"
+    var res = [String]()
+    
+    for i in stride(from: len-1, through: 0, by: -1) {
+        let a = A[i]
+        let b = B[i]
+        if a=="0" && b=="0"{
+            if temp=="1" {
+                res.append("1")
+            }else{
+                res.append("0")
+            }
+            temp = "0"
+        }else if (a=="1" && b=="0") || (a=="0" && b=="1"){
+            if temp=="1" {
+                res.append("0")
+                temp = "1"
+            }else{
+                res.append("1")
+                temp = "0"
+            }
+        }else if a == "1" && b == "1"{
+            if temp=="1" {
+                res.append("1")
+                temp = "1"
+            }else{
+                res.append("0")
+                temp = "1"
+            }
+        }
+    }
+    res.append(temp)
+    while res.last=="0"{
+        _ = res.removeLast()
+    }
+    if res.isEmpty{
+        print("0")
+    }else{
+        print(res.reversed().joined())
+    }
+}
