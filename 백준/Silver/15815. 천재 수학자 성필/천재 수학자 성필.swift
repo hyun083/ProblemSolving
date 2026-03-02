ed9d861b1b8203371d2bdc6f75c48a8e4bf1929e
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2026-03-02 22:03:59 +0900

    [Silver III] Title: 천재 수학자 성필, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.swift
+++ 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.swift
@@ -6,22 +6,13 @@ var stk = [Int]()
 for curr in line {
     if let num = Int(curr) {
         stk.append(num)
-    } else if curr == "+" {
+    } else {
         let op1 = stk.removeLast()
         let op2 = stk.removeLast()
-        stk.append(op2+op1)
-    } else if curr == "-" {
-        let op1 = stk.removeLast()
-        let op2 = stk.removeLast()
-        stk.append(op2-op1)
-    } else if curr == "*" {
-        let op1 = stk.removeLast()
-        let op2 = stk.removeLast()
-        stk.append(op2*op1)
-    } else if curr == "/" {
-        let op1 = stk.removeLast()
-        let op2 = stk.removeLast()
-        stk.append(op2/op1)
+        let res = curr=="+" ? op2+op1 :
+                  curr=="-" ? op2-op1 :
+                  curr=="*" ? op2*op1 : op2/op1
+        stk.append(res)
     }
 }
 print(stk.last!)
