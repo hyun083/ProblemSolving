39882c350ff74ef22af2f0b7d93a78de0b596446
Jeonghyun Lee <58415560+hyun083@users.noreply.github.com>
2026-03-26 22:48:38 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.swift
@@ -0,0 +1,11 @@
+while let line = readLine() {
+    if line == "animal" {
+        print("Panthera tigris")
+    } else if line == "tree" {
+        print("Pinus densiflora")
+    } else if line == "flower"{
+        print("Forsythia koreana")
+    } else {
+        break
+    }
+}
